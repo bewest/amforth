f1d352462a4bb11326b32ebf6e7b78a79fa111ed
mtrute <mtrute@1be2a6a9-e81f-0410-9be0-e59bd151596a>
2006-11-03 19:33:35 +0000

    compiles string constants
    git-svn-id: https://amforth.svn.sourceforge.net/svnroot/amforth/trunk@26 1be2a6a9-e81f-0410-9be0-e59bd151596a

--- words/dotstring.asm
+++ words/dotstring.asm
@@ -5,4 +5,31 @@ VE_DOTSTRING:
 XT_DOTSTRING:
     .dw DO_COLON
 PFA_DOTSTRING:
+    .dw XT_DOLITERAL
+    .dw $22
+    .dw XT_WORD
+    .dw XT_COUNT
+    .dw XT_SWAP
+    .dw XT_1MINUS
+    .dw XT_SWAP
+    .dw XT_2SLASH
+    .dw XT_1PLUS
+    .dw XT_DOLITERAL
+    .dw 0
+    .dw XT_DOLITERAL
+    .dw XT_DODOTSTRING
+    .dw XT_COMMA
+    .dw XT_DODO
+PFA_DOTSTRING1:
+	.dw XT_DOLITERAL
+	.dw '*'
+	.dw XT_EMIT
+    .dw XT_DUP
+    .dw XT_FETCH
+    .dw XT_COMMA
+    .dw XT_1PLUS
+    .dw XT_1PLUS
+    .dw XT_DOLOOP
+    .dw PFA_DOTSTRING1
+    .dw XT_DROP
     .dw XT_EXIT
