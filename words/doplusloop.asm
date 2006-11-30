c2124a780a6766bc83cab5aab5680f61fab2f130
mtrute <mtrute@1be2a6a9-e81f-0410-9be0-e59bd151596a>
2006-11-30 09:34:11 +0000

    bugfix for negative increments, thanks to Bernd Birkenbach for discovering the bug
    git-svn-id: https://amforth.svn.sourceforge.net/svnroot/amforth/trunk@55 1be2a6a9-e81f-0410-9be0-e59bd151596a

--- words/doplusloop.asm
+++ words/doplusloop.asm
@@ -13,14 +13,20 @@ PFA_DOPLUSLOOP:
     ld temp0, Y+
     add zl, temp0
     adc zh, temp1
-    pop temp1
-    pop temp0
-    cp zl, temp0
-    cpc zh, temp1
-    brge PFA_DOLOOP1 ; exit loop
+    pop temp3
+    pop temp2
+    cp zl, temp2
+    cpc zh, temp3
+    sbrs temp1, 7 ; if msb is set, increment is negative. in that case skip the next instruction
+    rjmp PFA_DOPLUSLOOP2 ; jump to test for positive overflow
+    brlt PFA_DOPLUSLOOP1 ; exit if underflow
+    rjmp PFA_DOPLUSLOOP3
+PFA_DOPLUSLOOP2:
+    brge PFA_DOPLUSLOOP1 ; exit if overflow
+PFA_DOPLUSLOOP3:
     ; next iteration
-    push temp0
-    push temp1
+    push temp2
+    push temp3
     push zl
     push zh
     rjmp PFA_DOBRANCH ; read next cell from dictionary and jump to its destination
