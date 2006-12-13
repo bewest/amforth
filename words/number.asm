35d1128bd1f25a0024ed34bf7a281e8959634233
mtrute <mtrute@1be2a6a9-e81f-0410-9be0-e59bd151596a>
2006-12-13 12:56:08 +0000

    respect minus sign
    git-svn-id: https://amforth.svn.sourceforge.net/svnroot/amforth/trunk@66 1be2a6a9-e81f-0410-9be0-e59bd151596a

--- words/number.asm
+++ words/number.asm
@@ -6,17 +6,38 @@ VE_NUMBER:
 XT_NUMBER:
     .dw DO_COLON
 PFA_NUMBER:
+    ; sign flag
+    .dw XT_DOLITERAL
+    .dw 1
+    .dw XT_SWAP
     .dw XT_ZERO
     .dw XT_SWAP
     .dw XT_COUNT
     .dw XT_ZERO
     .dw XT_DODO
-PFA_NUMBER1:
+PFA_NUMBER1: ; ( n a -- )
     .dw XT_DUP
     .dw XT_I
     .dw XT_PLUS
     .dw XT_CFETCH
     ; now check for +/- signs
+    .dw XT_DUP
+    .dw XT_DOLITERAL
+    .dw $2d ; '-'
+    .dw XT_EQUAL
+    .dw XT_DOCONDBRANCH
+    .dw PFA_NUMBER2
+    .dw XT_DROP
+    ; replace the sign flag
+    .dw XT_ROT
+    .dw XT_DROP
+    .dw XT_DOLITERAL
+    .dw -1
+    .dw XT_ROT
+    .dw XT_ROT
+    .dw XT_DOBRANCH
+    .dw PFA_NUMBER4
+PFA_NUMBER2:
     .dw XT_BASE
     .dw XT_FETCH
     .dw XT_DIGIT
@@ -33,7 +54,15 @@ PFA_NUMBER3:
     .dw XT_MUL
     .dw XT_PLUS
     .dw XT_SWAP
+PFA_NUMBER4:
     .dw XT_DOLOOP
     .dw PFA_NUMBER1
     .dw XT_DROP
+    ; incorporate sign into number
+    .dw XT_SWAP
+    .dw XT_LESSZERO
+    .dw XT_DOCONDBRANCH
+    .dw PFA_NUMBER5
+    .dw XT_NEGATE
+PFA_NUMBER5:
     .dw XT_EXIT
