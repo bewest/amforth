b428844deca1923b38d18a7fbe52fdb3b6557833
mtrute <mtrute@1be2a6a9-e81f-0410-9be0-e59bd151596a>
2006-12-16 20:05:48 +0000

    no comment
    git-svn-id: https://amforth.svn.sourceforge.net/svnroot/amforth/trunk@77 1be2a6a9-e81f-0410-9be0-e59bd151596a

--- devices/atmega8.asm
+++ devices/atmega8.asm
@@ -10,8 +10,20 @@
   .equ CELLSIZE = 2   ;
   .equ USERSIZE = 16  ; size of user area (at least 16 bytes)
 
-  .set PAGEMASK =  ~ ( PAGESIZE - 1 )
+  .equ PAGEMASK =  ~ ( PAGESIZE - 1 )
 
   .equ nrww = $0c00
   .equ codestart = $14
  
+  .equ UBRR0L = UBRRL
+  .equ UBRR0H = UBRRH 
+  .equ UCSR0C = UCSRC
+  .equ UCSR0B = UCSRB
+  .equ UDR0   = UDR
+  
+  .equ TXEN0  = TXEN
+  .equ RXEN0  = RXEN
+  .equ RXCIE0 = RXCIE
+  .equ UMSEL01 = URSEL
+  .equ UCSZ00  = UCSZ0
+  .equ UDRIE0  = UDRIE
