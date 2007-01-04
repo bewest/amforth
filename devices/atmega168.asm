466aadb92fc0f742480c7819220de23ccf96be6b
mtrute <mtrute@1be2a6a9-e81f-0410-9be0-e59bd151596a>
2007-01-04 10:04:58 +0000

    added interrupt table
    git-svn-id: https://amforth.svn.sourceforge.net/svnroot/amforth/trunk@93 1be2a6a9-e81f-0410-9be0-e59bd151596a

--- devices/atmega168.asm
+++ devices/atmega168.asm
@@ -19,3 +19,77 @@
   .equ SPMCR = SPMCSR
   .equ SPMEN = SELFPRGEN
   .equ EEMWE = EEMPE
+
+.org  INT0addr       
+    rjmp int0_isr  ; External Interrupt .orgest 0
+.org  INT1addr       
+    rjmp int1_isr  ; External Interrupt .orgest 1
+.org  PCI0addr        
+    reti  ; Pin Change Interrupt .orgest 0
+.org  PCI1addr        
+    reti  ; Pin Change Interrupt .orgest 0
+.org  PCI2addr        
+    reti  ; Pin Change Interrupt .orgest 1
+.org  WDTaddr         
+    reti  ; Watchdog Time-out Interrupt
+.org  OC2Aaddr        
+    reti  ; Timer/Counter2 Compare Match A
+.org  OC2Baddr        
+    reti  ; Timer/Counter2 Compare Match A
+.org  OVF2addr        
+    reti  ; Timer/Counter2 Overflow
+.org  ICP1addr        
+    reti  ; Timer/Counter1 Capture Event
+.org  OC1Aaddr        
+    reti  ; Timer/Counter1 Compare Match A
+.org  OC1Baddr        
+    reti  ; Timer/Counter1 Compare Match B
+.org  OVF1addr        
+    reti  ; Timer/Counter1 Overflow
+.org  OC0Aaddr        
+    reti  ; TimerCounter0 Compare Match A
+.org  OC0Baddr        
+    reti  ; TimerCounter0 Compare Match B
+.org  OVF0addr        
+    reti  ; Timer/Couner0 Overflow
+.org  SPIaddr         
+    reti  ; SPI Serial Transfer Complete
+.org  URXCaddr        
+    reti  ; USART Rx Complete
+.org  UDREaddr        
+    reti  ; USART, Data Register Empty
+.org  UTXCaddr        
+    reti  ; USART Tx Complete
+.org  ADCaddr         
+    rjmp adc_isr  ; ADC Conversion Complete
+.org  ERDYaddr        
+    reti  ; EEPROM Ready
+.org  ACIaddr         
+    reti  ; Analog Comparator
+.org  TWIaddr         
+    reti  ; Two-wire Serial Interface
+.org  SPMRaddr        
+    reti  ; Store Program Memory Read
+
+.org codestart
+; map avr interrupts to amforth interrupts
+int0_isr:
+    push yl
+    ldi yl, 0
+    rjmp intx_isr
+
+int1_isr:
+    push yl
+    ldi yl, 1
+    rjmp intx_isr
+
+adc_isr:
+    push yl
+    ldi yl, 2
+    rjmp intx_isr
+
+oc2_isr:
+    push yl
+    ldi yl, 3
+    rjmp intx_isr
+
