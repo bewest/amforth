e59223a7b70472bf43589712a8234f87f31e8c1a
mtrute <mtrute@1be2a6a9-e81f-0410-9be0-e59bd151596a>
2007-01-14 15:29:20 +0000

    exit is now invisble to re-enable atmega8
    git-svn-id: https://amforth.svn.sourceforge.net/svnroot/amforth/trunk@99 1be2a6a9-e81f-0410-9be0-e59bd151596a

--- words/exit.asm
+++ words/exit.asm
@@ -1,9 +1,9 @@
 ; ( -- ) 
 ; R: ( xt -- )
-VE_EXIT:
-    .db $04, "exit",0
-    .dw VE_HEAD
-    .set VE_HEAD = VE_EXIT
+;VE_EXIT:
+;    .db $04, "exit",0
+;    .dw VE_HEAD
+;    .set VE_HEAD = VE_EXIT
 XT_EXIT:
     .dw PFA_EXIT
 PFA_EXIT:
