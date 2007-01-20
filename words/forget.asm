bafbc261066b20f40302104476525318330bdffd
mtrute <mtrute@1be2a6a9-e81f-0410-9be0-e59bd151596a>
2007-01-20 09:43:57 +0000

    give back allocated memory, the body only for now
    git-svn-id: https://amforth.svn.sourceforge.net/svnroot/amforth/trunk@103 1be2a6a9-e81f-0410-9be0-e59bd151596a

--- words/forget.asm
+++ words/forget.asm
@@ -1,4 +1,4 @@
-; ( -- )
+; ( c<char> -- )
 
 VE_FORGET:
     .db $06, "forget",0
@@ -12,9 +12,13 @@ PFA_FORGET:
     .dw XT_FIND
     .dw XT_DOCONDBRANCH
     .dw PFA_FORGET1
-    .dw XT_1MINUS
-    .dw XT_IFETCH
+    .dw XT_1MINUS ; xt -> lfa
+    .dw XT_DUP
+    .dw XT_IFETCH ; 
     .dw XT_HEAD
+    .dw XT_ESTORE
+    
+    .dw XT_DP
     .dw XT_ESTORE
     .dw XT_EXIT
 
