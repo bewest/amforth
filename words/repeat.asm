145d29222c843773d018d0669a731f2b72f11251
mtrute <mtrute@1be2a6a9-e81f-0410-9be0-e59bd151596a>
2007-02-17 07:59:47 +0000

    repeat is much smaller now, thanks to Ulrich Hoffmann
    git-svn-id: https://amforth.svn.sourceforge.net/svnroot/amforth/trunk@123 1be2a6a9-e81f-0410-9be0-e59bd151596a

--- words/repeat.asm
+++ words/repeat.asm
@@ -7,10 +7,6 @@ VE_REPEAT:
 XT_REPEAT:
     .dw DO_COLON
 PFA_REPEAT:
-    .dw XT_DOLITERAL
-    .dw XT_DOBRANCH
-    .dw XT_COMMA
-    .dw XT_SWAP
-    .dw XT_LRESOLVE
+    .dw XT_AGAIN
     .dw XT_GRESOLVE
     .dw XT_EXIT
