934a8cbbb3d213f5093e53c1933854caa19ef36f
mtrute <mtrute@1be2a6a9-e81f-0410-9be0-e59bd151596a>
2007-02-19 08:17:28 +0000

    recoded r@ as assembler word, added new word compile. Thanks to Ulrich Hoffmann
    git-svn-id: https://amforth.svn.sourceforge.net/svnroot/amforth/trunk@127 1be2a6a9-e81f-0410-9be0-e59bd151596a

---
+++ words/compile.asm
@@ -0,0 +1,16 @@
+; ( -- )
+;VE_COMPILE:
+;    .db $07, "compile"
+;    .dw  VE_HEAD
+;    .set VE_HEAD = VE_COMPILE
+XT_COMPILE:
+    .dw DO_COLON
+PFA_COMPILE:
+    .dw XT_R_FROM
+    .dw XT_DUP
+    .dw XT_1PLUS
+    .dw XT_TO_R
+    .dw XT_IFETCH
+    .dw XT_COMMA
+    .dw XT_EXIT
+
