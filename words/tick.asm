0cc5025ae0b61fe0090b633b429c4fd1a67571c9
mtrute <mtrute@1be2a6a9-e81f-0410-9be0-e59bd151596a>
2007-02-21 18:59:13 +0000

    tick throws an exception -13 if word is not found
    git-svn-id: https://amforth.svn.sourceforge.net/svnroot/amforth/trunk@137 1be2a6a9-e81f-0410-9be0-e59bd151596a

--- words/tick.asm
+++ words/tick.asm
@@ -10,5 +10,11 @@ PFA_TICK:
     .dw XT_BL
     .dw XT_WORD
     .dw XT_FIND
-    .dw XT_DROP
+    .dw XT_EQUALZERO
+    .dw XT_DOCONDBRANCH
+    .dw PFA_TICK1
+    .dw XT_DOLITERAL
+    .dw -13
+    .dw XT_THROW
+PFA_TICK1:
     .dw XT_EXIT
