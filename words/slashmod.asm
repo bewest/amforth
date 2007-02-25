ba1d1b3be69b92e34872e697c705a83ba7ce2a7b
mtrute <mtrute@1be2a6a9-e81f-0410-9be0-e59bd151596a>
2007-02-25 16:07:08 +0000

    applied avr appnote 200 for signed division
    git-svn-id: https://amforth.svn.sourceforge.net/svnroot/amforth/trunk@158 1be2a6a9-e81f-0410-9be0-e59bd151596a

--- words/slashmod.asm
+++ words/slashmod.asm
@@ -7,57 +7,59 @@ VE_SLASHMOD:
 XT_SLASHMOD:
     .dw PFA_SLASHMOD
 PFA_SLASHMOD:
-    ld temp5, Y+
-    ld temp4, Y+
+    ld temp3, Y+
+    ld temp2, Y+
     
     ld temp1, Y+
     ld temp0, Y+
-    ldi temp3, 0
-    ldi temp2, 0
 
-;; unsigned 32/16 -> 16r16 divide
+    mov	temp6,temp1	;move dividend High to sign register
+    eor	temp6,temp3	;xor divisor High with sign register
+    sbrs	temp1,7	;if MSB in dividend set
+    rjmp	PFA_SLASHMOD_1
+    com	temp1		;    change sign of dividend
+    com	temp0		
+    subi	temp0,low(-1)
+    sbci	temp0,high(-1)
+PFA_SLASHMOD_1:	sbrs	temp3,7	;if MSB in divisor set
+    rjmp	PFA_SLASHMOD_2
+    com	temp3		;    change sign of divisor
+    com	temp2		
+    subi	temp2,low(-1)
+    sbci	temp3,high(-1)
+PFA_SLASHMOD_2:	clr	temp4	;clear remainder Low byte
+    sub	temp5,temp5;clear remainder High byte and carry
+    ldi	temp7,17	;init loop counter
 
-PFA_SLASHMODmod:
-
-  ; set loop counter
-    ldi temp6,$10
-
-PFA_SLASHMODmod_loop:
-    ; shift left, saving high bit
-    clr temp7
-    lsl temp0
-    rol temp1
-    rol temp2
-    rol temp3
-    rol temp7
-
-  ; try subtracting divisor
-    cp temp2, temp4
-    cpc temp3, temp5
-    cpc temp7,zerol
-
-    brcs PFA_SLASHMODmod_loop_control
-
-PFA_SLASHMODmod_subtract:
-    ; dividend is large enough
-    ; do the subtraction for real
-    ; and set lowest bit
-    inc temp0
-    sub temp2, temp4
-    sbc temp3, temp5
-
-PFA_SLASHMODmod_loop_control:
-    dec  temp6
-    brne PFA_SLASHMODmod_loop
+PFA_SLASHMOD_3:	rol	temp0		;shift left dividend
+    rol	temp1
+    dec	temp7		;decrement counter
+    brne	PFA_SLASHMOD_5		;if done
+    sbrs	temp6,7		;    if MSB in sign register set
+    rjmp	PFA_SLASHMOD_4
+    com	temp1	;        change sign of result
+    com	temp0
+    subi	temp0,low(-1)
+    sbci	temp1,high(-1)
+PFA_SLASHMOD_4:	rjmp PFA_SLASHMODmod_done			;    return
+PFA_SLASHMOD_5:	rol	temp4	;shift dividend into remainder
+    rol	temp5
+    sub	temp4,temp2	;remainder = remainder - divisor
+    sbc	temp5,temp3	;
+    brcc	PFA_SLASHMOD_6		;if result negative
+    add	temp4,temp2	;    restore remainder
+    adc	temp5,temp3
+    clc			;    clear carry to be shifted into result
+    rjmp	PFA_SLASHMOD_3		;else
+PFA_SLASHMOD_6:	sec			;    set carry to be shifted into result
+    rjmp	PFA_SLASHMOD_3
 
 PFA_SLASHMODmod_done:
     ; put remainder on stack
-    st -Y,temp2
-    st -Y,temp3
+    st -Y,temp4
+    st -Y,temp5
 
     ; put quotient on stack
     st -Y, temp0
     st -Y, temp1
     rjmp DO_NEXT
-
-
