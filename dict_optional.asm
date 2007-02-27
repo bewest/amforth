de925b4c23ea030233ae9ca3fb34936dcf5d52d9
mtrute <mtrute@1be2a6a9-e81f-0410-9be0-e59bd151596a>
2007-02-27 11:17:21 +0000

    added optional dictionary, put case \& co into it
    git-svn-id: https://amforth.svn.sourceforge.net/svnroot/amforth/trunk@163 1be2a6a9-e81f-0410-9be0-e59bd151596a

---
+++ dict_optional.asm
@@ -0,0 +1,7 @@
+; this dictionary contains optional words
+; they may be moved to the core dictionary if needed
+
+.include "words/optional/case.asm"
+.include "words/optional/of.asm"
+.include "words/optional/endof.asm"
+.include "words/optional/endcase.asm"
