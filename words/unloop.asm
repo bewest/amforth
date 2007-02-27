9bc07101f37ebca48b5806ab1942c584d642c3a0
mtrute <mtrute@1be2a6a9-e81f-0410-9be0-e59bd151596a>
2007-02-27 10:50:57 +0000

    (unloop) is now unloop
    git-svn-id: https://amforth.svn.sourceforge.net/svnroot/amforth/trunk@160 1be2a6a9-e81f-0410-9be0-e59bd151596a

---
+++ words/unloop.asm
@@ -0,0 +1,14 @@
+; ( -- )
+; R(loop-sys -- )
+VE_UNLOOP:
+    .db 6, "unloop", 0
+    .dw VE_HEAD
+    .set VE_HEAD = VE_UNLOOP
+XT_UNLOOP:
+    .dw PFA_UNLOOP
+PFA_UNLOOP:
+    pop temp1
+    pop temp0
+    pop temp1
+    pop temp0
+    rjmp DO_NEXT
