a7fafd94f98d55a08a8fb1d56f15b978b80d4b0a
mtrute <mtrute@1be2a6a9-e81f-0410-9be0-e59bd151596a>
2007-03-02 08:13:25 +0000

    word uses free ram space above heap, pad is too small
    git-svn-id: https://amforth.svn.sourceforge.net/svnroot/amforth/trunk@165 1be2a6a9-e81f-0410-9be0-e59bd151596a

--- words/word.asm
+++ words/word.asm
@@ -34,7 +34,8 @@ PFA_WORD2:
     .dw XT_DOCONDBRANCH
     .dw PFA_WORD3
     ; character can be stored to PAD
-    .dw XT_PAD
+    .dw XT_HEAP
+    .dw XT_EFETCH
     .dw XT_R_FROM
     .dw XT_1PLUS
     .dw XT_DUP
@@ -54,8 +55,10 @@ PFA_WORD3:
     .dw PFA_WORD2
 PFA_WORD6:
     .dw XT_R_FROM
-    .dw XT_PAD
+    .dw XT_HEAP
+    .dw XT_EFETCH
     .dw XT_CSTORE
     .dw XT_DROP    
-    .dw XT_PAD
+    .dw XT_HEAP
+    .dw XT_EFETCH
     .dw XT_EXIT
