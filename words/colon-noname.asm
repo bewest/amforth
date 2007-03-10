366510c9705017c706048b07622c485c4800f545
mtrute <mtrute@1be2a6a9-e81f-0410-9be0-e59bd151596a>
2007-03-10 13:04:09 +0000

    added :noname, removed itype
    git-svn-id: https://amforth.svn.sourceforge.net/svnroot/amforth/trunk@174 1be2a6a9-e81f-0410-9be0-e59bd151596a

---
+++ words/colon-noname.asm
@@ -0,0 +1,15 @@
+; ( -- )
+; R( -- ) 
+VE_COLONNONAME:
+    .db $7, ":noname"
+    .dw VE_HEAD
+    .set VE_HEAD = VE_COLONNONAME
+XT_COLONNONAME:
+    .dw DO_COLON
+PFA_COLONNONAME:
+    .dw XT_HERE
+    .dw XT_DOLITERAL
+    .dw DO_COLON
+    .dw XT_COMMA
+    .dw XT_RBRACKET
+    .dw XT_EXIT
