b70cc2df370b9fae3cf2444894c3b645242212a9
mtrute <mtrute@1be2a6a9-e81f-0410-9be0-e59bd151596a>
2007-03-23 12:34:32 +0000

    replaced up with up@/up\!
    git-svn-id: https://amforth.svn.sourceforge.net/svnroot/amforth/trunk@197 1be2a6a9-e81f-0410-9be0-e59bd151596a

--- words/up.asm
+++ words/up.asm
@@ -1,12 +1,25 @@
 ; ( -- addr )
 ; R( -- )
-VE_UP:
-    .db $02, "up",0
+VE_UP_FETCH:
+    .db $03, "up@"
     .dw VE_HEAD
-    .set VE_HEAD = VE_UP
-XT_UP:
-    .dw PFA_UP
-PFA_UP:
+    .set VE_HEAD = VE_UP_FETCH
+XT_UP_FETCH:
+    .dw PFA_UP_FETCH
+PFA_UP_FETCH:
     st -Y, upl
     st -Y, uph
+    rjmp DO_NEXT
+
+; ( addr -- )
+; R( -- )
+VE_UP_STORE:
+    .db $03, "up!"
+    .dw VE_HEAD
+    .set VE_HEAD = VE_UP_STORE
+XT_UP_STORE:
+    .dw PFA_UP_STORE
+PFA_UP_STORE:
+    ld uph, Y+
+    ld upl, Y+
     rjmp DO_NEXT
