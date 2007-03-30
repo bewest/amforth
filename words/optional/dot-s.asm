2fd629d67e40374e99caccf5b2099c481689b34b
mtrute <mtrute@1be2a6a9-e81f-0410-9be0-e59bd151596a>
2007-03-30 14:21:57 +0000

    re-imported old words: .s and idump as optional
    git-svn-id: https://amforth.svn.sourceforge.net/svnroot/amforth/trunk@210 1be2a6a9-e81f-0410-9be0-e59bd151596a

---
+++ words/optional/dot-s.asm
@@ -0,0 +1,36 @@
+; ( -- )
+; R( -- )
+; stack dump
+VE_DOTS:
+    .db $02, ".s",0
+    .dw VE_HEAD
+    .set VE_HEAD = VE_DOTS
+XT_DOTS:
+    .dw DO_COLON
+PFA_DOTS:
+    .dw XT_SP_FETCH
+    .dw XT_DEPTH
+    .dw XT_1MINUS
+    .dw XT_QDUP
+    .dw XT_DOCONDBRANCH
+    .dw PFA_DOTS2
+    .dw XT_ZERO
+    .dw XT_DODO
+PFA_DOTS1:
+    .dw XT_DUP
+    .dw XT_I
+    .dw XT_DUP
+    .dw XT_DOT
+    .dw XT_2STAR
+    .dw XT_PLUS
+    .dw XT_DUP
+    .dw XT_DOT
+    .dw XT_FETCH 
+    .dw XT_BYTESWAP
+    .dw XT_DOT
+    .dw XT_CR
+    .dw XT_DOLOOP
+    .dw PFA_DOTS1
+PFA_DOTS2:
+    .dw XT_DROP
+    .dw XT_EXIT
---
+++ words/optional/idump.asm
@@ -0,0 +1,27 @@
+; ( addr len -- )
+; R( -- )
+; dumps flash memory beginning with address addr and len cells long
+;    aaaa    0  1  2  3
+;    aaaa+4  4  5  6  7
+VE_IDUMP:
+    .db $5, "idump"
+    .dw VE_HEAD
+    .set VE_HEAD = VE_IDUMP
+XT_IDUMP:
+    .dw DO_COLON
+PFA_IDUMP:
+    .dw XT_ZERO
+    .dw XT_DODO
+PFA_IDUMP1:
+    .dw XT_I
+    .dw XT_OVER
+    .dw XT_PLUS
+	.dw XT_DUP
+	.dw XT_DOT
+    .dw XT_IFETCH
+    .dw XT_DOT    
+    .dw XT_CR
+    .dw XT_DOLOOP
+    .dw PFA_IDUMP1
+    .dw XT_DROP
+    .dw XT_EXIT
