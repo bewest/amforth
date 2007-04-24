43b26d10b2bfdfc1afdb85acc313df01a75db2fd
mtrute <mtrute@1be2a6a9-e81f-0410-9be0-e59bd151596a>
2007-04-24 18:43:29 +0000

    new word: sleep
    git-svn-id: https://amforth.svn.sourceforge.net/svnroot/amforth/trunk@262 1be2a6a9-e81f-0410-9be0-e59bd151596a

---
+++ words/sleep.asm
@@ -0,0 +1,12 @@
+; ( n1 -- n2 ) Arithmetics
+; R( -- )
+; optimized decrement (CORE)
+VE_SLEEP:
+    .db $05, "sleep"
+    .dw VE_HEAD
+    .set VE_HEAD = VE_SLEEP
+XT_SLEEP:
+    .dw PFA_SLEEP
+PFA_SLEEP:
+    sleep
+    rjmp DO_NEXT
