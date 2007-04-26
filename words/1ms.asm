03cf2e8c636fae44df42b9718dca08bc5ee4b6c0
mtrute <mtrute@1be2a6a9-e81f-0410-9be0-e59bd151596a>
2007-04-26 17:39:20 +0000

    slightly more accurate delay
    git-svn-id: https://amforth.svn.sourceforge.net/svnroot/amforth/trunk@270 1be2a6a9-e81f-0410-9be0-e59bd151596a

--- words/1ms.asm
+++ words/1ms.asm
@@ -1,6 +1,6 @@
-; (  -- ) Time
-; R( -- )
-; busy waits exactly 1 milli second
+; ( -- ) Time
+; R(--)
+; busy waits (almost) exactly 1 millisecond
 VE_1MS:
     .db $03, "1ms"
     .dw VE_HEAD
@@ -13,11 +13,10 @@ PFA_1MS:
     .set delay_cycles = cpu_frequency / 1000
     ldi zl, LOW( delay_cycles / 4 )
     ldi zh, HIGH(delay_cycles / 4 )
-    sbiw zl, 2
+    sbiw zl, 42 ; internal plus forth kernel overhead
 PFA_1MS1:
     sbiw zl, 1
     brne PFA_1MS1
     rjmp DO_NEXT
 
-; millisecods: 1ms is needed for organizing and housekeeping
-; : ms 1- 0 do 1ms loop ;
+; : ms 0 do 1ms loop ;
