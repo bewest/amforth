19f6e8aabaef73b19683d54f80c3290dd5a327bb
mtrute <mtrute@1be2a6a9-e81f-0410-9be0-e59bd151596a>
2007-05-18 13:22:01 +0000

    experimental defer variants
    git-svn-id: https://amforth.svn.sourceforge.net/svnroot/amforth/trunk@294 1be2a6a9-e81f-0410-9be0-e59bd151596a

--- blocks/defer.fs
+++ blocks/defer.fs
@@ -1,34 +1,22 @@
 \ *******************************************
-\ defer operates in EEPROM. See proposal
+\  defer operates in varios memory areas 
+\  ( EEPROM, RAM, USER )
 \ *******************************************
 
 \ include "postpone.frt"
+\ include "body.frt"
 
-: defer ( "name" -- )
-      create  
-      edp e@ 
-      dup ,             \ save in dictionary
-      dup 1+ 1+ edp e!  \ allot 1 cell in EEPROM
-      ['] abort swap e! \ default action is abort
-   does> ( i*x -- j*x ) i@ e@ execute 
-; 
-
-: defer@ ( xt1 -- xt2 )
-    1+ i@ e@
+\ ****************************
+\ generic words
+\ ****************************
+\ xt2 is the word associated to word xt1
+: defer@ ( xt1 -- xt2 ) 
+    dup   ( -- xt xt)
+    >body ( -- xt pfa )
+    1+    ( -- xt 'xt-dodefer@ )
+    i@    ( -- xt xt-dodefer@ )
+    execute 
 ;
-
-: defer! ( xt2 xt1 -- )
-    1+ i@ e!
-;
-
-: is ( x <name> -- )
-    state @   
-    if 
-	postpone ['] postpone defer!
-    else
-	' defer!
-    then
-; immediate 
 
 : action-of
     state @
@@ -38,3 +26,87 @@
 	' defer@
     then
 ; immediate
+
+\ set the word xt1 to execute the word xt2
+: defer! ( xt1 xt2 -- ) \ part of minimal
+    dup   ( -- xt1 xt2 xt2 )
+    >body ( -- xt1 xt2 pfa )
+    2 +   ( -- xt1 'xt-dodefer! )
+    i@    ( -- xt1 xt-dodefer! )
+    execute 
+;
+
+: is ( x <name> -- ) \ part of minimal
+    state @   
+    if 
+	postpone ['] postpone defer!
+    else
+	' defer!
+    then
+; immediate 
+
+\ ****************************
+\ EEPROM defers
+\ ****************************
+: Edefer@ ( xt1 -- xt2 )
+    >body i@ e@
+;
+
+: Edefer! ( xt2 xt1 -- )
+    >body i@ e!
+;
+
+: Edefer ( "name" -- )
+      create  
+      edp e@ 
+      dup ,             \ save in dictionary
+      ['] Edefer@ ,
+      ['] Edefer! ,
+      dup 1+ 1+ edp e!  \ allot 1 cell in EEPROM
+      ['] abort swap e! \ default action is abort
+   does> ( i*x -- j*x ) 
+      i@ e@ execute
+; 
+
+\ ***************************
+\ USER defered 
+\ **************************
+: Udefer@ ( xt1 -- xt2 )
+    1+ i@ up@ + @
+;
+
+: Udefer! ( xt2 xt1 -- )
+    1+ i@ up@ + !
+;
+
+
+: Udefer ( u "name" -- )
+      user
+      ['] Udefer@ ,
+      ['] Udefer! ,
+   does> ( i*x -- j*x ) 
+      @ execute 
+;
+
+\ ***************************
+\ RAM defered
+\ ***************************
+
+: Rdefer@ ( xt1 -- xt2 )
+    1+ i@ @
+;
+
+: Rdefer! ( xt2 xt1 -- )
+    1+ i@ !
+;
+
+: Rdefer ( "name" -- )
+      create
+      heap e@ 
+      dup ,              \ save in dictionary
+      2 allot            \ allot 1 cell in RAM
+      ['] abort swap !   \ default action is abort
+      ['] Rdefer@ ,
+      ['] Rdefer! ,
+   does> ( i*x -- j*x ) i@ @ execute 
+; 
