9676018b218bc8d718c2dab21e81e42249c9918d
mtrute <mtrute@1be2a6a9-e81f-0410-9be0-e59bd151596a>
2007-06-10 11:41:14 +0000

    smaller but slower
    git-svn-id: https://amforth.svn.sourceforge.net/svnroot/amforth/trunk@325 1be2a6a9-e81f-0410-9be0-e59bd151596a

--- words/doliteral.asm
+++ words/doliteral.asm
@@ -6,13 +6,22 @@
 ;    .dw VE_HEAD
 ;    .set VE_HEAD = VE_DOLITERAL
 XT_DOLITERAL:
-    .dw PFA_DOLITERAL
+    .dw DO_COLON
 PFA_DOLITERAL:
-    savetos
-    movw zl, xl
-    lsl zl
-    rol zh
-    lpm tosl, Z+
-    lpm tosh, Z
-    adiw xl, 1
-    rjmp DO_NEXT
+    .dw XT_R_FROM
+    .dw XT_DUP
+    .dw XT_1PLUS
+    .dw XT_TO_R
+    .dw XT_IFETCH
+    .dw XT_EXIT
+
+;    .dw PFA_DOLITERAL
+;PFA_DOLITERAL:
+;    savetos
+;    movw zl, xl
+;    lsl zl
+;    rol zh
+;    lpm tosl, Z+
+;    lpm tosh, Z
+;    adiw xl, 1
+;    rjmp DO_NEXT
