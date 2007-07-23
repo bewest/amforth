dd1d5d850c2636c98e7568832a327a3fa550b3ba
mtrute <mtrute@1be2a6a9-e81f-0410-9be0-e59bd151596a>
2007-07-23 14:21:26 +0000

    re-coded find / icompare
    git-svn-id: https://amforth.svn.sourceforge.net/svnroot/amforth/trunk@359 1be2a6a9-e81f-0410-9be0-e59bd151596a

--- words/find.asm
+++ words/find.asm
@@ -1,4 +1,4 @@
-; ( addr --  -- [ addr 0 ] | [ xt [-1|1]] ) Tools
+; ( addr -- [ addr 0 ] | [ xt [-1|1]] ) Tools
 ; R( -- )
 ; search dictionary
 VE_FIND:
@@ -11,123 +11,62 @@ PFA_FIND:
     .dw XT_DUP
     .dw XT_TO_R
     .dw XT_HEAD
-    .dw XT_EFETCH
+    .dw XT_EFETCH  ; ( -- addr ve-start )
 PFA_FIND1:
-    ; ( addr )
-    .dw XT_DUP
-    .dw XT_NOTEQUALZERO
+    ; ( addr-ram addr-flash )
+    .dw XT_DUP     
+    .dw XT_NOTEQUALZERO 
     .dw XT_DOCONDBRANCH
-    .dw PFA_FIND2
-    .dw XT_ICOMPARE
-    ; (addr-ram addr-flash -- addr-flash' 0|1
-    .dw XT_QDUP
-    .dw XT_NOTEQUALZERO
+    .dw PFA_FIND2   ; end-of-list signature (ve-start == 0) -> skip to end
+    .dw XT_SWAP     ; ( -- addr-flash addr-ram )
+    .dw XT_OVER     ; ( -- addr-flash addr-ram addr-flash )
+    .dw XT_ICOMPARE ; ( -- addr-flash f)
     .dw XT_DOCONDBRANCH
     .dw PFA_FIND3
     ; we found the word
+    ; first read the i-flag
+    .dw XT_DUP    ; ( -- addr-flash addr-flash )
+    .dw XT_IFETCH
+    .dw XT_DOLITERAL
+    .dw $0080
+    .dw XT_AND    ; i-flag
+    .dw XT_DOLITERAL
+    .dw -1
     .dw XT_SWAP
-    .dw XT_1PLUS ; make XT
+    .dw XT_DOCONDBRANCH
+    .dw PFA_FIND4
+    .dw XT_NEGATE
+PFA_FIND4:
     .dw XT_SWAP
+    ; now calculate the xt
     .dw XT_R_FROM
-    .dw XT_DROP
-
+    .dw XT_CFETCH ; ( -- addr-flash len)
+    .dw XT_2SLASH
+    .dw XT_1PLUS
+    .dw XT_PLUS 
+    .dw XT_1PLUS
+    .dw XT_SWAP
     .dw XT_EXIT
 
 PFA_FIND3:
-    .dw XT_R_FROM
-    .dw XT_DUP
-    .dw XT_TO_R
-    .dw XT_SWAP
+    ; this entry does not match, go to the next one
+    .dw XT_DUP   ; ( -- addr-flash addr-flash )
     .dw XT_IFETCH
-    .dw XT_DOBRANCH
-    .dw PFA_FIND1
-
-PFA_FIND2:
-    .dw XT_DROP
-    .dw XT_DROP
-    .dw XT_R_FROM
-    .dw XT_ZERO
-    .dw XT_EXIT
-
-; private headerless routine
-; compares counted string in RAM with counted string in flash
-; 
-; ( addr --  -- [ addr 0 ] | [ xt [-1|1]] )
-; R( -- )
-; search dictionary
-;VE_ICOMPARE:
-;    .db $04, "icompare"
-;    .dw VE_HEAD
-;    .set VE_HEAD = VE_ICOMPARE
-XT_ICOMPARE:
-    .dw DO_COLON
-PFA_ICOMPARE:
-    ; ( addr-ram addr-flash -- 0| +/-1
-    .dw XT_DUP
-    .dw XT_IFETCH
-    .dw XT_DUP
-    .dw XT_DOLITERAL
-    .dw $0080
-    .dw XT_AND
-    .dw XT_TO_R           ; send immediate flag to r-stack
-    .dw XT_ZERO
-    .dw XT_TO_R
     .dw XT_DOLITERAL
     .dw $001f
     .dw XT_AND
     .dw XT_2SLASH
     .dw XT_1PLUS
-    .dw XT_TO_R
-PFA_ICOMPARE1:
-    .dw XT_OVER
-    .dw XT_OVER
-    .dw XT_IFETCH
-    .dw XT_DOLITERAL
-    .dw $ff7f
-    .dw XT_AND
-    .dw XT_SWAP
-    .dw XT_FETCH
-    .dw XT_EQUAL
-    .dw XT_DOCONDBRANCH
-    .dw PFA_ICOMPARE3
-    ; increment pointers, 1 CELL for FLASH, 2 bytes for RAM
-    .dw XT_1PLUS
-    .dw XT_SWAP
-    .dw XT_1PLUS
-    .dw XT_1PLUS
-    .dw XT_SWAP
-    ; decrement cell counter, leave loop for zero
-    .dw XT_R_FROM
-    .dw XT_1MINUS
-    .dw XT_DUP
-    .dw XT_TO_R
-    .dw XT_EQUALZERO
-    .dw XT_DOCONDBRANCH
-    .dw PFA_ICOMPARE2
-    ; we found matching strings
-    .dw XT_R_FROM
-    .dw XT_R_FROM
-    .dw XT_DROP
-    .dw XT_DOLITERAL
-    .dw 1
-    .dw XT_TO_R
-    .dw XT_TO_R
-    .dw XT_DOBRANCH
-    .dw PFA_ICOMPARE3
-PFA_ICOMPARE2:
-    .dw XT_DOBRANCH
-    .dw PFA_ICOMPARE1
-
-PFA_ICOMPARE3:
-    .dw XT_SWAP
-    .dw XT_DROP
-    .dw XT_R_FROM
     .dw XT_PLUS
+    .dw XT_IFETCH    ; ( -- ve-start')
+    .dw XT_R_FETCH
+    .dw XT_SWAP
+    .dw XT_DOBRANCH
+    .dw PFA_FIND1    ; check next word
+
+PFA_FIND2: ; clean up, word not found.
+    .dw XT_DROP
+    .dw XT_DROP
     .dw XT_R_FROM
-    .dw XT_R_FROM
-    .dw XT_EQUALZERO
-    .dw XT_DOCONDBRANCH
-    .dw PFA_ICOMPARE4
-    .dw XT_NEGATE
-PFA_ICOMPARE4:
+    .dw XT_ZERO
     .dw XT_EXIT
---
+++ words/icompare.asm
@@ -0,0 +1,65 @@
+; ( addr-ram addr-flash  --  f)
+; R( -- )
+; compares counted string in RAM with counted string in flash
+;VE_ICOMPARE:
+;    .db $08, "icompare", 0
+;    .dw VE_HEAD
+;    .set VE_HEAD = VE_ICOMPARE
+XT_ICOMPARE:
+    .dw DO_COLON
+PFA_ICOMPARE:
+    ; a string has at least 1 character, length 0 is not possible
+    .dw XT_OVER     ; ( -- a-r a-f a-r)
+    .dw XT_FETCH    ; ( -- a-r a-f c-r)
+    .dw XT_OVER     ; ( -- a-r a-f c-r a-f)
+    .dw XT_IFETCH   ; ( -- a-r a-f c-r c-f)
+    .dw XT_DOLITERAL
+    .dw $ff7f       ; mask out immediate bit
+    .dw XT_AND
+    .dw XT_NOTEQUAL ; ( -- a-r a-f f)
+    .dw XT_DOCONDBRANCH
+    .dw PFA_ICOMPARE1
+    .dw XT_DROP
+    .dw XT_DROP
+    .dw XT_ZERO
+    .dw XT_EXIT
+
+PFA_ICOMPARE1:
+    ; the two strings have the same length an the same first character
+    ; ( addr-ram addr-flash -- )
+    .dw XT_OVER   ; ( -- addr-ram addr-flash addr-ram )
+    .dw XT_CFETCH ; ( -- addr-ram addr-flash char-len )
+    .dw XT_2SLASH ; ( -- addr-ram addr-flash cell-len' )
+    .dw XT_ZERO   ; ( -- addr-ram addr-flash cell-len' 0 )
+    .dw XT_DOQDO
+    .dw PFA_ICOMPARE3
+PFA_ICOMPARE2:
+    ; ( addr-ram addr-flash -- )
+    .dw XT_1PLUS
+    .dw XT_SWAP
+    .dw XT_1PLUS
+    .dw XT_1PLUS  ; ( -- addr-flash addr-ram )
+    .dw XT_OVER   ; ( -- addr-flash addr-ram addr-flash)
+    .dw XT_IFETCH
+    .dw XT_OVER   
+    .dw XT_FETCH  ; ( -- addr-flash addr-ram data-flash data-ram)
+    .dw XT_NOTEQUAL
+    .dw XT_DOCONDBRANCH
+    .dw PFA_ICOMPARE4
+    ; the strings are different -> leave the loop
+    .dw XT_UNLOOP
+    .dw XT_DROP
+    .dw XT_DROP
+    .dw XT_ZERO
+    .dw XT_EXIT
+PFA_ICOMPARE4:
+    .dw XT_SWAP  ; ( -- addr-ram addr-flash )
+    .dw XT_DOLOOP
+    .dw PFA_ICOMPARE2
+PFA_ICOMPARE3:
+    .dw XT_DROP
+    .dw XT_DROP
+    .dw XT_DOLITERAL
+    .dw -1
+    .dw XT_EXIT
+    
