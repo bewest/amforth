1486296bc6c75a5a65511c80ee484294ef343d36
mtrute <mtrute@1be2a6a9-e81f-0410-9be0-e59bd151596a>
2007-10-03 18:48:27 +0000

    new word [char]
    git-svn-id: https://amforth.svn.sourceforge.net/svnroot/amforth/trunk@401 1be2a6a9-e81f-0410-9be0-e59bd151596a

---
+++ words/bracketchar.asm
@@ -0,0 +1,19 @@
+; ( -- c ) Tools
+; R( -- )
+; first character of the next word
+VE_BRACKETCHAR:
+    .db $86, "[char]",0
+    .dw VE_HEAD
+    .set VE_HEAD = VE_BRACKETCHAR
+XT_BRACKETCHAR:
+    .dw DO_COLON
+PFA_BRACKETCHAR:
+    .dw XT_BL
+    .dw XT_WORD
+    .dw XT_COUNT
+    .dw XT_DROP
+    .dw XT_CFETCH
+    .dw XT_COMPILE
+    .dw XT_DOLITERAL
+    .dw XT_COMMA
+    .dw XT_EXIT
