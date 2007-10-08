ba928f1d1e60e047ab15e5a9e9c7c7a27ff090e8
mtrute <mtrute@1be2a6a9-e81f-0410-9be0-e59bd151596a>
2007-10-08 07:18:27 +0000

    new word: fill from core
    git-svn-id: https://amforth.svn.sourceforge.net/svnroot/amforth/trunk@406 1be2a6a9-e81f-0410-9be0-e59bd151596a

---
+++ words/fill.asm
@@ -0,0 +1,28 @@
+; ( c-addr u c -- ) CORE EXT
+; R( -- )
+; Fill memory beginning at a-addr with character u
+VE_FILL:
+    .db $04, "fill",0
+    .dw VE_HEAD
+    .set VE_HEAD = VE_FILL
+XT_FILL:
+    .dw DO_COLON
+PFA_FILL:
+    .dw XT_SWAP
+    .dw XT_ZERO
+    .dw XT_DOQDO
+    .dw PFA_FILL2
+PFA_FILL1:
+    
+    .dw XT_SWAP ; ( -- c c-addr)
+    .dw XT_OVER
+    .dw XT_OVER
+    .dw XT_CSTORE  ; ( -- c c-addr)
+    .dw XT_1PLUS
+    .dw XT_SWAP
+    .dw XT_DOLOOP
+    .dw PFA_FILL1
+PFA_FILL2:
+    .dw XT_DROP
+    .dw XT_DROP
+    .dw XT_EXIT
