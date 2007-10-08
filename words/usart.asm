7c7431450e20df5577767df9d9040d59b9de79d2
mtrute <mtrute@1be2a6a9-e81f-0410-9be0-e59bd151596a>
2007-10-08 07:17:47 +0000

    removed baud rate calculation, factored io redirect
    git-svn-id: https://amforth.svn.sourceforge.net/svnroot/amforth/trunk@405 1be2a6a9-e81f-0410-9be0-e59bd151596a

--- words/usart.asm
+++ words/usart.asm
@@ -16,12 +16,32 @@ PFA_BAUD00:          ; ( -- )
 ; R( --)
 ; initialize usart0
 VE_USART0:
-  .db $06, "usart0",0
+  .db 7, "+usart0"
   .dw VE_HEAD
   .set VE_HEAD = VE_USART0
 XT_USART0:
   .dw DO_COLON
 PFA_USART0:          ; ( -- )
+  .dw XT_DUP
+  .dw XT_BYTESWAP
+  .dw XT_DOLITERAL
+  .dw BAUDRATE0_HIGH
+  .dw XT_CSTORE
+  .dw XT_DOLITERAL
+  .dw BAUDRATE0_LOW
+  .dw XT_CSTORE
+
+  .dw XT_DOLITERAL
+  .dw USART0_C_VALUE
+  .dw XT_DOLITERAL
+  .dw USART0_C
+  .dw XT_CSTORE
+  .dw XT_DOLITERAL
+  .dw USART0_B_VALUE
+  .dw XT_DOLITERAL
+  .dw USART0_B
+  .dw XT_CSTORE
+
   .dw XT_ZERO
   .dw XT_DOLITERAL
   .dw usart0_tx_in
@@ -42,66 +62,48 @@ PFA_USART0:          ; ( -- )
   .dw usart0_rx_out
   .dw XT_CSTORE
 
-  .dw XT_F_CPU
-  .dw XT_D2SLASH
-  .dw XT_D2SLASH
-  .dw XT_D2SLASH
-  .dw XT_D2SLASH
-  .dw XT_ROT
-  .dw XT_UMSLASHMOD
-  .dw XT_SWAP
-  .dw XT_DROP
-  .dw XT_1MINUS
+  .dw XT_EXIT
 
-  .dw XT_DUP
-  .dw XT_BYTESWAP
-  .dw XT_DOLITERAL
-  .dw BAUDRATE0_HIGH
-  .dw XT_CSTORE
-  .dw XT_DOLITERAL
-  .dw BAUDRATE0_LOW
-  .dw XT_CSTORE
-  .dw XT_DOLITERAL
-  .dw USART0_C_VALUE
-  .dw XT_DOLITERAL
-  .dw USART0_C
-  .dw XT_CSTORE
-  .dw XT_DOLITERAL
-  .dw USART0_B_VALUE
-  .dw XT_DOLITERAL
-  .dw USART0_B
-  .dw XT_CSTORE
+; ( -- ) Hardware Access
+; R( --)
+; calculate the baudrate register value
+VE_TOUSART0:
+  .db 7, ">usart0"
+  .dw VE_HEAD
+  .set VE_HEAD = VE_TOUSART0
+XT_TOUSART0:
+  .dw DO_COLON
+PFA_TOUSART0:          ; ( -- )
   
-    ; set IO
-    .dw XT_DOLITERAL
-    .dw XT_TX0
-    .dw XT_DOLITERAL
-    .dw XT_EMIT
-    .dw XT_DEFERSTORE
+  .dw XT_DOLITERAL
+  .dw XT_TX0
+  .dw XT_DOLITERAL
+  .dw XT_EMIT
+  .dw XT_DEFERSTORE
 
-    .dw XT_DOLITERAL
-    .dw XT_TX0Q
-    .dw XT_DOLITERAL
-    .dw XT_EMITQ
-    .dw XT_DEFERSTORE
+  .dw XT_DOLITERAL
+  .dw XT_TX0Q
+  .dw XT_DOLITERAL
+  .dw XT_EMITQ
+  .dw XT_DEFERSTORE
 
-    .dw XT_DOLITERAL
-    .dw XT_RX0
-    .dw XT_DOLITERAL
-    .dw XT_KEY
-    .dw XT_DEFERSTORE
+  .dw XT_DOLITERAL
+  .dw XT_RX0
+  .dw XT_DOLITERAL
+  .dw XT_KEY
+  .dw XT_DEFERSTORE
 
-    .dw XT_DOLITERAL
-    .dw XT_RX0Q
-    .dw XT_DOLITERAL
-    .dw XT_KEYQ
-    .dw XT_DEFERSTORE
+  .dw XT_DOLITERAL
+  .dw XT_RX0Q
+  .dw XT_DOLITERAL
+  .dw XT_KEYQ
+  .dw XT_DEFERSTORE
 
-    .dw XT_DOLITERAL
-    .dw XT_NOOP
-    .dw XT_DOLITERAL
-    .dw XT_SLASHKEY
-    .dw XT_DEFERSTORE
+  .dw XT_DOLITERAL
+  .dw XT_NOOP
+  .dw XT_DOLITERAL
+  .dw XT_SLASHKEY
+  .dw XT_DEFERSTORE
 
   .dw XT_EXIT
 
