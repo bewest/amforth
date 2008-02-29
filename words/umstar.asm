ed372e0c3ef5fb8755367ca1f7bc3bd23eae8289
mtrute <mtrute@1be2a6a9-e81f-0410-9be0-e59bd151596a>
2008-02-29 20:29:27 +0000

    m* was in fact a um*
    git-svn-id: https://amforth.svn.sourceforge.net/svnroot/amforth/trunk@487 1be2a6a9-e81f-0410-9be0-e59bd151596a

---
+++ words/umstar.asm
@@ -0,0 +1,36 @@
+; ( u1 u2 -- d) Arithmetics
+; R( -- )
+; multiply 2 unsigned cells to a double cell
+VE_UMSTAR:
+    .db $03, "um*"
+    .dw VE_HEAD
+    .set VE_HEAD = VE_UMSTAR
+XT_UMSTAR:
+    .dw PFA_UMSTAR
+PFA_UMSTAR:
+    movw temp0, tosl
+    loadtos
+    ; result: (temp3*temp1)* 65536 + (temp3*temp0 + temp1*temp2) * 256 + (temp0 * temp2)
+    ; low bytes
+    mul tosl,temp0
+    movw zl, r0
+    clr temp2
+    clr temp3
+    ; middle bytes
+    mul tosh, temp0
+    add zh, r0
+    adc temp2, r1
+    adc temp3, zeroh
+        
+    mul tosl, temp1
+    add zh, r0
+    adc temp2, r1
+    adc temp3, zeroh
+    
+    mul tosh, temp1
+    add temp2, r0
+    adc temp3, r1
+    movw tosl, zl
+    savetos
+    movw tosl, temp2
+    rjmp DO_NEXT
