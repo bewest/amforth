0c519a0318225e4d1ee193321ef2026a56e8db1c
mtrute <mtrute@1be2a6a9-e81f-0410-9be0-e59bd151596a>
2008-06-07 15:11:27 +0000

    MCU register name differs between various MCUs
    git-svn-id: https://amforth.svn.sourceforge.net/svnroot/amforth/trunk@542 1be2a6a9-e81f-0410-9be0-e59bd151596a

--- core/words/no-jtag.asm
+++ core/words/no-jtag.asm
@@ -9,9 +9,20 @@ XT_NOJTAG:
     .dw PFA_NOJTAG
 PFA_NOJTAG:
     cli
+    .if defined MCUCSR
     in_ temp0, MCUCSR
+    .else
+    in_ temp0, MCUCR
+    .endif
+
     ori temp0, ( 1<<JTD)
+
+    .if defined MCUCSR
     out_ MCUCSR, temp0
     out_ MCUCSR, temp0     ; needs to be done twice within 4 cycles
+    .else
+    out_ MCUCR, temp0
+    out_ MCUCR, temp0     ; needs to be done twice within 4 cycles
+    .endif
     sei
     jmp_ DO_NEXT
