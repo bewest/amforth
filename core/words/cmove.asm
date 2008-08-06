e6273573937ac2c0a79a2a7e4fd82294442827ac
mtrute <mtrute@1be2a6a9-e81f-0410-9be0-e59bd151596a>
2008-08-06 07:31:08 +0000

    added cmove primitive
    git-svn-id: https://amforth.svn.sourceforge.net/svnroot/amforth/trunk@593 1be2a6a9-e81f-0410-9be0-e59bd151596a

---
+++ core/words/cmove.asm
@@ -0,0 +1,34 @@
+; (addr-from addr-to n -- ) Memory
+; R( -- )
+; copy data in RAM
+VE_CMOVE:
+    .db $05, "cmove"
+    .dw VE_HEAD
+    .set VE_HEAD  = VE_CMOVE
+XT_CMOVE:
+    .dw PFA_CMOVE
+PFA_CMOVE:
+    push xh
+    push xl
+    movw wl, tosl
+    ld zl, Y+
+    ld zh, Y+ ; addr-to
+    ld xl, Y+
+    ld xh, Y+ ; addr-from
+    mov temp0, wh
+    or temp0, wl
+    brbs 1, PFA_CMOVE1
+    add zl, wl
+    adc zh, wh
+    add xl, wl
+    adc xh, wh
+PFA_CMOVE2:
+    ld temp1, X+
+    st Z+, temp1
+    sbiw wl, 1
+    brbc 1, PFA_CMOVE2
+PFA_CMOVE1:
+    pop xl
+    pop xh
+    loadtos
+    rjmp DO_NEXT
