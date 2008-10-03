e2928343e376f661b5259bff823f0800bd5ee326
mtrute <mtrute@1be2a6a9-e81f-0410-9be0-e59bd151596a>
2008-10-03 08:51:00 +0000

    Register B is a duplicate of register A
    git-svn-id: https://amforth.svn.sourceforge.net/svnroot/amforth/trunk@599 1be2a6a9-e81f-0410-9be0-e59bd151596a

---
+++ core/words/reg-b.asm
@@ -0,0 +1,170 @@
+; ( -- n2 ) Extended VM
+; R( -- )
+; Read memory pointed to by register B (Extended VM)
+VE_BFETCH:
+    .db $02, "b@",0
+    .dw VE_HEAD
+    .set VE_HEAD = VE_BFETCH
+XT_BFETCH:
+    .dw PFA_BFETCH
+PFA_BFETCH:
+    savetos
+    movw zl, bl
+    ld tosl, Z+
+    ld tosh, Z+
+    rjmp DO_NEXT
+
+; ( n1 -- n2 ) Extended VM
+; R( -- )
+; Read memory pointed to by register B plus offset (Extended VM)
+VE_NBFETCH:
+    .db $03, "nb@"
+    .dw VE_HEAD
+    .set VE_HEAD = VE_NBFETCH
+XT_NBFETCH:
+    .dw PFA_NBFETCH
+PFA_NBFETCH:
+    movw zl, bl
+    add zl, tosl
+    adc zh, bh
+    ld tosl, Z+
+    ld tosh, Z+
+    rjmp DO_NEXT
+
+; ( -- n ) Extended VM
+; R( -- )
+; Read memory pointed to by register B, increment B by 1 cell (Extended VM)
+VE_BFETCHPLUS:
+    .db $03, "b@+"
+    .dw VE_HEAD
+    .set VE_HEAD = VE_BFETCHPLUS
+XT_BFETCHPLUS:
+    .dw PFA_BFETCHPLUS
+PFA_BFETCHPLUS:
+    savetos
+    movw zl, bl
+    ld tosl, Z+
+    ld tosh, Z+
+    movw bl, zl
+    rjmp DO_NEXT
+
+; ( -- n ) Extended VM
+; R( -- )
+; Read memory pointed to by register B, decrement B by 1 cell (Extended VM)
+VE_BFETCHMINUS:
+    .db $03, "b@-"
+    .dw VE_HEAD
+    .set VE_HEAD = VE_BFETCHMINUS
+XT_BFETCHMINUS:
+    .dw PFA_BFETCHMINUS
+PFA_BFETCHMINUS:
+    savetos
+    movw zl, bl
+    ld tosl, -Z
+    ld tosh, -Z
+    movw bl, zl
+    rjmp DO_NEXT
+
+; ( n -- ) Extended VM
+; R( -- )
+; Write memory pointed to by register B (Extended VM)
+VE_BSTORE:
+    .db $02, "b!",0
+    .dw VE_HEAD
+    .set VE_HEAD = VE_BSTORE
+XT_BSTORE:
+    .dw PFA_BSTORE
+PFA_BSTORE:
+    movw zl, bl
+    st Z+, tosl
+    st Z+, tosh
+    loadtos
+    rjmp DO_NEXT
+
+; ( n offs -- ) Extended VM
+; R( -- )
+; Write memory pointed to by register B plus offset (Extended VM)
+VE_NBSTORE:
+    .db $03, "nb!"
+    .dw VE_HEAD
+    .set VE_HEAD = VE_NBSTORE
+XT_NBSTORE:
+    .dw PFA_NBSTORE
+PFA_NBSTORE:
+    movw zl, bl
+    add zl, tosl
+    adc zh, tosh
+    loadtos
+    st Z+, tosl
+    st Z+, tosh
+    loadtos
+    rjmp DO_NEXT
+
+; ( -- n2 ) Extended VM
+; R( -- )
+; Write memory pointed to by register B, increment B by 1 cell (Extended VM)
+VE_BSTOREPLUS:
+    .db $03, "b!+"
+    .dw VE_HEAD
+    .set VE_HEAD = VE_BSTOREPLUS
+XT_BSTOREPLUS:
+    .dw PFA_BSTOREPLUS
+PFA_BSTOREPLUS:
+    movw zl, bl
+    st Z+, tosl
+    st Z+, tosh
+    loadtos
+    movw bl, zl
+    rjmp DO_NEXT
+
+; ( -- n2 ) Extended VM
+; R( -- )
+; Write memory pointed to by register B, decrement B by 1 cell (Extended VM)
+VE_BSTOREMINUS:
+    .db $03, "b!-"
+    .dw VE_HEAD
+    .set VE_HEAD = VE_BSTOREMINUS
+XT_BSTOREMINUS:
+    .dw PFA_BSTOREMINUS
+PFA_BSTOREMINUS:
+    movw zl, bl
+    st -Z, tosl
+    st -Z, tosh
+    loadtos
+    movw bl, zl
+    rjmp DO_NEXT
+
+
+
+; ( n -- ) Extended VM
+; R( -- )
+; Write to B register (Extended VM)
+VE_TO_B:
+    .db $02, ">b",0
+    .dw VE_HEAD
+    .set VE_HEAD = VE_TO_B
+XT_TO_B:
+    .dw PFA_TO_B
+PFA_TO_B:
+    movw bl, tosl
+    loadtos
+    rjmp DO_NEXT
+
+; ( n1 -- n2 ) Extended VM
+; R( -- )
+; read the B register (Extended VM)
+VE_B_FROM:
+    .db $02, "b>",0
+    .dw VE_HEAD
+    .set VE_HEAD = VE_B_FROM
+XT_B_FROM:
+    .dw PFA_B_FROM
+PFA_B_FROM:
+    savetos
+    movw tosl, bl
+    rjmp DO_NEXT
+
+; for more information read
+; http://www.complang.tuwien.ac.at/anton/euroforth/ef08/papers/pelc.pdf
+;  adapted index based access from X/Y registers
+;    note: offset is byte address, not cell!
