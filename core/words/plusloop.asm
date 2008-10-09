6eb800aef9fe391d7ef2a496b3d735645fb3e443
mtrute <mtrute@1be2a6a9-e81f-0410-9be0-e59bd151596a>
2008-10-09 16:13:53 +0000

    renamed internal labels as suggested by Michael Kalus
    git-svn-id: https://amforth.svn.sourceforge.net/svnroot/amforth/trunk@604 1be2a6a9-e81f-0410-9be0-e59bd151596a

--- core/words/plusloop.asm
+++ core/words/plusloop.asm
@@ -1,13 +1,13 @@
 ; ( addr -- ) Control Structure
 ; R( -- )
 ; compile +loop and resolve branch
-VE_PLOOP:
+VE_PLUSLOOP:
     .db $85, "+loop"
     .dw VE_HEAD
-    .set VE_HEAD = VE_PLOOP
-XT_PLOOP:
+    .set VE_HEAD = VE_PLUSLOOP
+XT_PLUSLOOP:
     .dw DO_COLON
-PFA_PLOOP:
+PFA_PLUSLOOP:
     .dw XT_COMPILE
     .dw XT_DOPLUSLOOP
     .dw XT_LRESOLVE
