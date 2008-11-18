425aac8afb1e55f6c97db511d033fde10327886d
mtrute <mtrute@1be2a6a9-e81f-0410-9be0-e59bd151596a>
2008-11-18 19:22:53 +0000

    New word: WITHIN
    git-svn-id: https://amforth.svn.sourceforge.net/svnroot/amforth/trunk@661 1be2a6a9-e81f-0410-9be0-e59bd151596a

---
+++ core/words/within.asm
@@ -0,0 +1,20 @@
+; ( n1 n2 -- n3 ) Arithmetics
+; R( -- )
+; multiply routine
+VE_WITHIN:
+    .dw $ff06
+    .db "within"
+    .dw VE_HEAD
+    .set VE_HEAD = VE_WITHIN
+XT_WITHIN:
+    .dw DO_COLON
+PFA_WITHIN:
+    .dw XT_OVER
+    .dw XT_MINUS
+    .dw XT_TO_R
+    .dw XT_MINUS
+    .dw XT_R_FROM
+    .dw XT_ULESS
+    .dw XT_EXIT
+
+; : WITHIN ( n min max -- Truth )  OVER - >R - R> U< ;
