6a3f429fa70f80754170114d432f6d4a98014c4c
mtrute <mtrute@1be2a6a9-e81f-0410-9be0-e59bd151596a>
2008-11-23 20:36:45 +0000

    properly handle last character
    git-svn-id: https://amforth.svn.sourceforge.net/svnroot/amforth/trunk@663 1be2a6a9-e81f-0410-9be0-e59bd151596a

--- core/words/itype.asm
+++ core/words/itype.asm
@@ -9,23 +9,35 @@ VE_ITYPE:
 XT_ITYPE:
     .dw DO_COLON
 PFA_ITYPE:
+    .dw XT_DUP    ; ( --addr len len)
+    .dw XT_2SLASH ; ( -- addr len len/2
+    .dw XT_SWAP   ; ( -- addr len/2 len
+    .dw XT_OVER   ; ( -- addr len/2 len len/2
+    .dw XT_2STAR  ; ( -- addr len/2 len len'
+    .dw XT_MINUS  ; ( -- addr len/2 rem
+    .dw XT_TO_R
     .dw XT_ZERO
     .dw XT_DOQDO
-    .dw PFA_ITYPE_DONE
-PFA_ITYPE_NEXT:
-      ; ( -- addr )
-      .dw XT_DUP
+    .dw PFA_ITYPE2
+PFA_ITYPE1:
+    .dw XT_DUP         ; ( -- addr addr )
+    .dw XT_IFETCH      ; ( -- addr c1c2 )
+    .dw XT_DUP
+    .dw XT_EMIT
+    .dw XT_HIEMIT
+    .dw XT_1PLUS    ; ( -- addr+cell )
+    .dw XT_DOLOOP
+    .dw PFA_ITYPE1
+PFA_ITYPE2:
+    .dw XT_R_FROM
+    .dw XT_GREATERZERO
+    .dw XT_DOCONDBRANCH
+    .dw PFA_ITYPE3
+      .dw XT_DUP     ; well, tricky
       .dw XT_IFETCH
-      .dw XT_DUP
       .dw XT_EMIT
-      .dw XT_HIEMIT
-      .dw XT_1PLUS
-    .dw XT_DOLITERAL
-    .dw 2
-    .dw XT_DOPLUSLOOP
-    .dw PFA_ITYPE_NEXT
-PFA_ITYPE_DONE:
-    .dw XT_DROP
+PFA_ITYPE3:
+    .dw XT_DROP        ; ( -- )
     .dw XT_EXIT
 
 ; ( w -- )
