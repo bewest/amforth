b726bbf1bfdcf54a37daef15b6e88fa9fa708290
mtrute <mtrute@1be2a6a9-e81f-0410-9be0-e59bd151596a>
2008-11-29 12:27:10 +0000

    show the current cpu frequency
    git-svn-id: https://amforth.svn.sourceforge.net/svnroot/amforth/trunk@667 1be2a6a9-e81f-0410-9be0-e59bd151596a

--- appl/eval-pollin/words/applturnkey.asm
+++ appl/eval-pollin/words/applturnkey.asm
@@ -14,4 +14,17 @@ PFA_APPLTURNKEY:
     .dw XT_TOUSART0
     .dw XT_INTON
     .dw XT_VER
+    .dw XT_F_CPU
+    .dw XT_DOLITERAL
+    .dw 1000
+    .dw XT_UMSLASHMOD
+    .dw XT_SWAP
+    .dw XT_DROP
+    .dw XT_DECIMAL
+    .dw XT_DOT
+    .dw XT_SLITERAL
+    .dw 3
+    .db "kHz"
+    .dw XT_ITYPE
+    
     .dw XT_EXIT
