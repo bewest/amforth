cd313f6f4a430e8576f977bcc0fde63899b9fa18
mtrute <mtrute@1be2a6a9-e81f-0410-9be0-e59bd151596a>
2008-11-29 12:29:04 +0000

    adapt code to new core requirements
    git-svn-id: https://amforth.svn.sourceforge.net/svnroot/amforth/trunk@669 1be2a6a9-e81f-0410-9be0-e59bd151596a

--- appl/template/template.asm
+++ appl/template/template.asm
@@ -1,11 +1,11 @@
 ; This is a template for an amforth project.
 ;
-; The order of the entries (esp the include order) must not be 
-; changed since it is very important that the settings are in the 
+; The order of the entries (esp the include order) must not be
+; changed since it is very important that the settings are in the
 ; right order
 ;
 ; first is to include the macros from the amforth
-; directory 
+; directory
 .include "macros.asm"
 
 ; include the amforth device definition file. These
@@ -14,16 +14,16 @@
 
 ; amforth needs two essential parameters
 ; cpu clock in hertz, 1MHz is factory default
-.equ cpu_frequency = 8000000
+.equ F_CPU = 8000000
 
 ; initial baud rate of terminal
-.equ baud_rate = 9600
+.equ BAUD = 9600
 
 .equ HLDSIZE  = $10 ; 16 bit cellsize with binary representation
 .equ TIBSIZE  = $64 ; ANS94 needs at least 80 characters per line
 .equ USERSIZE = 24  ; size of user area in bytes, at least 24
 
-; addresses of various data segments 
+; addresses of various data segments
 .set heap = ramstart           ; start address of HEAP, grows upward
 .set rstackstart = RAMEND      ; start address of return stack, grows downward
 .set stackstart  = RAMEND - 80 ; start address of data stack, grows downward
