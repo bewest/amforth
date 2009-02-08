8826b0a6626138e216ad8670c21d7629ca8cab86
mtrute <mtrute@1be2a6a9-e81f-0410-9be0-e59bd151596a>
2009-02-08 09:23:03 +0000

    minus sign is only checked at the first character of the string, otherwise an error -13 will be thrown
    git-svn-id: https://amforth.svn.sourceforge.net/svnroot/amforth/trunk@706 1be2a6a9-e81f-0410-9be0-e59bd151596a

--- core/words/number.asm
+++ core/words/number.asm
@@ -9,38 +9,32 @@ VE_NUMBER:
 XT_NUMBER:
     .dw DO_COLON
 PFA_NUMBER:
-    ; sign flag
+    .dw XT_COUNT   ; ( -- addr len )
+    .dw XT_OVER    ; ( -- addr char addr )
+    .dw XT_CFETCH
+    ; now check for +/- signs
     .dw XT_DOLITERAL
-    .dw 1             ; ( -- addr 1)
-    .dw XT_ZERO       ; ( -- addr 1 0)
-    .dw XT_ROT        ; ( -- 1 0 addr)
-    .dw XT_COUNT      ; ( -- 1 0 addr' len)
-    .dw XT_ZERO       ; ( -- 1 0 addr' len 0)
-    .dw XT_DODO
+    .dw $2d ; '-'
+    .dw XT_EQUAL  ; ( -- addr len flag )
+    .dw XT_DUP
+    .dw XT_TO_R   ; save the is-negative flag
+    .dw XT_DOCONDBRANCH
+    .dw PFA_NUMBER0
+    .dw XT_DOLITERAL      ; skip sign character
+    .dw 1
+    .dw XT_SLASHSTRING
+PFA_NUMBER0: ; ( addr len  -- )
+    .dw XT_ZERO       ; starting value
+    .dw XT_ROT
+    .dw XT_ROT
+    .dw XT_ZERO       ; ( -- n addr' len 0)
+    .dw XT_DOQDO
     .dw PFA_NUMBER6
-PFA_NUMBER1: ; ( -- sign n addr )
+PFA_NUMBER1: ; ( -- n addr )
     .dw XT_DUP
     .dw XT_I
     .dw XT_PLUS
-    .dw XT_CFETCH  ; ( -- sign n addr char )
-    ; now check for +/- signs
-    .dw XT_DUP
-    .dw XT_DOLITERAL
-    .dw $2d ; '-'
-    .dw XT_EQUAL
-    .dw XT_DOCONDBRANCH
-    .dw PFA_NUMBER2
-    .dw XT_DROP    ; ( -- sign n addr )
-    ; replace the sign flag
-    .dw XT_ROT
-    .dw XT_DROP
-    .dw XT_DOLITERAL
-    .dw -1
-    .dw XT_ROT
-    .dw XT_ROT
-    .dw XT_DOBRANCH
-    .dw PFA_NUMBER4
-PFA_NUMBER2:
+    .dw XT_CFETCH  ; ( -- n addr char )
     .dw XT_DIGITQ
     .dw XT_EQUALZERO
     ; check for non number characters and stop converting if found
@@ -50,20 +44,19 @@ PFA_NUMBER2:
     .dw -13
     .dw XT_THROW
 PFA_NUMBER3:
-    .dw XT_ROT
+    .dw XT_ROT   ; ( -- addr digit n )
     .dw XT_BASE
     .dw XT_FETCH
     .dw XT_STAR
     .dw XT_PLUS
-    .dw XT_SWAP
+    .dw XT_SWAP  ; ( -- n addr )
 PFA_NUMBER4:
     .dw XT_DOLOOP
     .dw PFA_NUMBER1
 PFA_NUMBER6:
     .dw XT_DROP
     ; incorporate sign into number
-    .dw XT_SWAP
-    .dw XT_LESSZERO
+    .dw XT_R_FROM
     .dw XT_DOCONDBRANCH
     .dw PFA_NUMBER5
     .dw XT_NEGATE
