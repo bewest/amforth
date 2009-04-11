b865a83553b3fc7a2a54130b1321081a9bdd6a2e
mtrute <mtrute@1be2a6a9-e81f-0410-9be0-e59bd151596a>
2009-04-11 18:26:41 +0000

    undo some code changes, going back to WORD
    git-svn-id: https://amforth.svn.sourceforge.net/svnroot/amforth/trunk@739 1be2a6a9-e81f-0410-9be0-e59bd151596a

--- core/words/lparenthesis.asm
+++ core/words/lparenthesis.asm
@@ -9,22 +9,20 @@ VE_LPARENTHESIS:
 XT_LPARENTHESIS:
     .dw DO_COLON
 PFA_LPARENTHESIS:
-    .dw XT_G_IN
-    .dw XT_FETCH
     .dw XT_DOLITERAL
     .dw $29
-    .dw XT_PARSE
+    .dw XT_WORD
+    .dw XT_COUNT
     .dw XT_SWAP
-    .dw XT_DROP
-    .dw XT_PLUS
-    .dw XT_G_IN
-    .dw XT_FETCH
-    .dw XT_EQUAL
+    .dw XT_EQUALZERO
     .dw XT_DOCONDBRANCH
     .dw PFA_LPARENTHESIS3
+    .dw XT_DROP
     .dw XT_REFILL
     .dw XT_EQUALZERO
     .dw XT_DOCONDBRANCH
     .dw PFA_LPARENTHESIS
 PFA_LPARENTHESIS3:
+    .dw XT_G_IN
+    .dw XT_PLUSSTORE
     .dw XT_EXIT
