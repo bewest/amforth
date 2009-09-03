fc7c347abf6ddaf95af462e39bd128ba9c22f334
mtrute <mtrute@1be2a6a9-e81f-0410-9be0-e59bd151596a>
2009-09-03 10:35:22 +0000

    receive characters with polling only, disbaling the receive interrupt is a must however
    git-svn-id: https://amforth.svn.sourceforge.net/svnroot/amforth/trunk@769 1be2a6a9-e81f-0410-9be0-e59bd151596a

---
+++ core/words/usart-rx-poll.asm
@@ -0,0 +1,53 @@
+; (c -- ) Hardware Access
+; R( --)
+; put 1 character into output queue, wait if needed, enable UDRIE interrupt
+VE_RX:
+    .dw $ff02
+    .db "rx"
+    .dw VE_HEAD
+    .set VE_HEAD = VE_RX
+XT_RX:
+    .dw DO_COLON
+PFA_RX:
+  ; wait for data ready
+  .dw XT_RXQ
+  .dw XT_DOCONDBRANCH
+  .dw PFA_RX
+  ; send to usart
+  .dw XT_DOLITERAL
+  .dw USART_DATA
+  .dw XT_CFETCH
+  .dw XT_EXIT
+
+; ( -- f)  Hardware Access
+; R( --)
+; check if a character can be appended to output queue.
+VE_RXQ:
+    .dw $ff03
+    .db "rx?",0
+    .dw VE_HEAD
+    .set VE_HEAD = VE_RXQ
+XT_RXQ:
+    .dw DO_COLON
+PFA_RXQ:
+  .dw XT_PAUSE
+  .dw XT_DOLITERAL
+  .dw USART_A
+  .dw XT_CFETCH
+  .dw XT_DOLITERAL
+  .dw USART_RX_bm
+  .dw XT_AND
+  .dw XT_EXIT
+
+; ( -- ) Hardware Access
+; R( --)
+; initialize usart
+;VE_USART_INIT_RX:
+;  .dw $ff06
+;  .db "+usart"
+;  .dw VE_HEAD
+;  .set VE_HEAD = VE_USART_INIT_RX
+XT_USART_INIT_RX:
+  .dw DO_COLON
+PFA_USART_INIT_RX:          ; ( -- )
+  .dw XT_EXIT
