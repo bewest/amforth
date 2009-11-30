e708b1567de48b28588d7be701bf9d114764b2a4
mtrute <mtrute@1be2a6a9-e81f-0410-9be0-e59bd151596a>
2009-11-30 10:13:47 +0000

    setup the ATXmega application
    git-svn-id: https://amforth.svn.sourceforge.net/svnroot/amforth/trunk@811 1be2a6a9-e81f-0410-9be0-e59bd151596a

---
+++ appl/atxmega/atxmega.asm
@@ -0,0 +1,42 @@
+.include "macros.asm"
+.include "devices/atxmega128a1.asm"
+
+  .equ HLDSIZE  = $10 ; 16 bit cellsize with binary representation
+  .equ TIBSIZE  = $64 ; 80 characters is one line...
+  .equ CELLSIZE = 2   ; never change
+  .equ APPUSERSIZE = 0  ; size of user area
+
+  .equ TERM_USART = USARTE0_base
+  .equ TERM_USART_PORT = PORTE_base
+
+  .equ USART_DATA = TERM_USART + USART_DATA_offset
+  .equ USART_A = TERM_USART + USART_CTRLA_offset
+  .equ USART_B = TERM_USART + USART_CTRLB_offset
+  .equ USART_C = TERM_USART + USART_CTRLC_offset
+  .equ BAUDRATE_HIGH = TERM_USART + USART_BAUDCTRLB_offset
+  .equ BAUDRATE_LOW = TERM_USART + USART_BAUDCTRLA_offset
+;  .equ TERM_USART_DreAddr = USARTE0_DRE_vect
+;  .equ URXCAddr = USARTE0_RXC_vect
+
+  .equ USART_A_VALUE = 0
+  .equ USART_B_VALUE = USART_TXEN_bm | USART_RXEN_bm
+  .equ USART_C_VALUE = USART_CHSIZE_8BIT_gc
+
+.equ NUMWORDLISTS = 8
+
+; cpu clock in hertz
+.equ F_CPU = 2000000
+; baud rate of terminal
+.equ BAUD = 9600
+
+.set dict_appl=1
+.set istore_nrww=1
+
+.set heap = ramstart
+.set rstackstart = RAMEND
+.set stackstart  = RAMEND - 80
+
+.set OPTION_FLOWCONTROL = 1
+.set amforth_interpreter = $F000
+
+.include "amforth-low.asm"
