0cd214e3804e35bde8c269f01ddb12e6a5d8cbd2
mtrute <mtrute@1be2a6a9-e81f-0410-9be0-e59bd151596a>
2010-02-01 19:41:50 +0000

    check stack after executing not before, reformatting and indentation
    git-svn-id: https://amforth.svn.sourceforge.net/svnroot/amforth/trunk@828 1be2a6a9-e81f-0410-9be0-e59bd151596a

--- core/words/interpret.asm
+++ core/words/interpret.asm
@@ -10,7 +10,6 @@ XT_INTERPRET:
     .dw DO_COLON
 PFA_INTERPRET:
 PFA_INTERPRET1:
-    .dw XT_QSTACK
     .dw XT_BL
     .dw XT_WORD
     .dw XT_DUP
@@ -26,18 +25,16 @@ PFA_INTERPRET1:
     .dw XT_DOCONDBRANCH 
     .dw PFA_INTERPRET2
     .dw XT_NUMBER
-    ; check state
     .dw XT_STATE
     .dw XT_FETCH
     .dw XT_DOCONDBRANCH
     .dw PFA_INTERPRET9
-    ; 
-    .dw XT_COMPILE
-    .dw XT_DOLITERAL
-    .dw XT_COMMA
+        .dw XT_COMPILE
+        .dw XT_DOLITERAL
+        .dw XT_COMMA
 PFA_INTERPRET9:
-    .dw XT_DOBRANCH
-    .dw PFA_INTERPRET3
+        .dw XT_DOBRANCH
+        .dw PFA_INTERPRET3
 PFA_INTERPRET2:
     ; either compile or execute
     .dw XT_GREATERZERO
@@ -63,6 +60,7 @@ PFA_INTERPRET7:
 PFA_INTERPRET8:
 PFA_INTERPRET6:
 PFA_INTERPRET3:
+    .dw XT_QSTACK
     .dw XT_DOBRANCH
     .dw PFA_INTERPRET1
 
