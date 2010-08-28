8b680b65f8727d69f38e5d0d64e944b857387ea0
mtrute <mtrute@1be2a6a9-e81f-0410-9be0-e59bd151596a>
2010-08-28 11:46:57 +0000

    general update: more recent mcu, other usert settings, generally 10 bytes USER space
    git-svn-id: https://amforth.svn.sourceforge.net/svnroot/amforth/trunk@929 1be2a6a9-e81f-0410-9be0-e59bd151596a

--- appl/template/template.asm
+++ appl/template/template.asm
@@ -4,6 +4,8 @@
 ; changed since it is very important that the settings are in the
 ; right order
 ;
+; note: .set is like a variable, .equ is like a constant
+;
 ; first is to include the macros from the amforth
 ; directory
 .include "macros.asm"
@@ -16,14 +18,25 @@
 ; cpu clock in hertz, 1MHz is factory default
 .equ F_CPU = 8000000
 
+; terminal settings
+.set WANT_ISR_RX = 1 ; interrupt driven receive
+.set WANT_ISR_TX = 0 ; send slowly but with less code space
+
 ; initial baud rate of terminal
-.include "drivers/usart.asm"
 .equ BAUD = 9600
-.equ USART_B_VALUE = (1<<TXEN) | (1<<RXEN) | (1<<RXCIE)
-.equ USART_C_VALUE = (3<<UCSZ0)
+.set USART_B_VALUE = (1<<TXEN0) | (1<<RXEN0)
+
+.if WANT_ISR_RX == 1
+  .set USART_B_VALUE = (1<<TXEN0) | (1<<RXEN0)| (1<<RXCIE0)
+.else
+  .set USART_B_VALUE = (1<<TXEN0) | (1<<RXEN0)
+.endif
+; 8N1 is commonly used
+.equ USART_C_VALUE = (3<<UCSZ00)
+.include "drivers/usart_0.asm"
 
 .equ TIBSIZE  = $64    ; ANS94 needs at least 80 characters per line
-.equ APPUSERSIZE = 0   ; size of application specific user area in bytes
+.equ APPUSERSIZE = 10  ; size of application specific user area in bytes
 
 ; addresses of various data segments
 .set here = ramstart           ; initial HERE at compile time, grows upward
