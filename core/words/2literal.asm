b1f1a32d87b822786937dc9787d1e608eacb6494
mtrute <mtrute@1be2a6a9-e81f-0410-9be0-e59bd151596a>
2010-08-29 11:24:24 +0000

    new 2literal: compiles a double cell number in colon words
    git-svn-id: https://amforth.svn.sourceforge.net/svnroot/amforth/trunk@934 1be2a6a9-e81f-0410-9be0-e59bd151596a

---
+++ core/words/2literal.asm
@@ -0,0 +1,20 @@
+; ( d -- ) Compiler
+; R( -- )
+; compile a double cell literal in colon defintions
+VE_2LITERAL:
+    .dw $0008
+    .db "2literal"
+    .dw VE_HEAD
+    .set VE_HEAD = VE_2LITERAL
+XT_2LITERAL:
+    .dw DO_COLON
+PFA_2LITERAL:
+    .dw XT_SWAP
+    .dw XT_COMPILE
+    .dw XT_DOLITERAL
+    .dw XT_COMMA
+    .dw XT_COMPILE
+    .dw XT_DOLITERAL
+    .dw XT_COMMA
+    
+    .dw XT_EXIT
