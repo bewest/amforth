b80433fffec26feded5cd8fca2d0c632f3214a49
mtrute <mtrute@1be2a6a9-e81f-0410-9be0-e59bd151596a>
2011-04-30 17:34:29 +0000

    tos should be the first element of the array, not the last one
    git-svn-id: https://amforth.svn.sourceforge.net/svnroot/amforth/trunk@1009 1be2a6a9-e81f-0410-9be0-e59bd151596a

--- core/words/get-ee-array.asm
+++ core/words/get-ee-array.asm
@@ -14,17 +14,22 @@ PFA_GET_EE_ARRAY:
     .dw XT_TO_R    ; save the counter to r-stack
     .dw XT_R_FETCH
     .dw XT_ZERO
+    .dw XT_SWAP    ; go from bigger to smaller addresses
     .dw XT_DOQDO
     .dw PFA_GET_EE_ARRAY2
 PFA_GET_EE_ARRAY1:
     ; ( ee-addr )
-    .dw XT_CELLPLUS
-    .dw XT_DUP
-    .dw XT_FETCHE
-    .dw XT_SWAP
-    .dw XT_DOLOOP
+    .dw XT_I
+    .dw XT_CELLS ; ( -- ee-addr i*2 )
+    .dw XT_OVER  ; ( -- ee-addr i*2 ee-addr )
+    .dw XT_PLUS  ; ( -- ee-addr ee-addr+i
+    .dw XT_FETCHE ;( -- ee-addr item_i )
+    .dw XT_SWAP   ;( -- item_i ee-addr )
+    .dw XT_TRUE  ; shortcut for -1
+    .dw XT_DOPLUSLOOP
     .dw PFA_GET_EE_ARRAY1
 PFA_GET_EE_ARRAY2:
+    .dw XT_DROP
     .dw XT_DROP
     .dw XT_R_FROM ; get the counter from r-stack
     .dw XT_EXIT
