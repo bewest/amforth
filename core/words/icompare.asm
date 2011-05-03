f85267c17f2deb104e22c5da38752a4f7f47dc4a
mtrute <mtrute@1be2a6a9-e81f-0410-9be0-e59bd151596a>
2011-05-03 17:30:17 +0000

    ignore case if want-option at compile time is set
    git-svn-id: https://amforth.svn.sourceforge.net/svnroot/amforth/trunk@1011 1be2a6a9-e81f-0410-9be0-e59bd151596a

--- core/words/icompare.asm
+++ core/words/icompare.asm
@@ -29,9 +29,11 @@ PFA_ICOMPARE_LOOP:
     ; ( r-addr f-addr --)
     .dw XT_OVER
     .dw XT_FETCH
+.ifdef WANT_IGNORECASE == 1
+    .dw XT_ICOMPARE_LC
+.endif
     .dw XT_OVER
     .dw XT_FETCHI ; ( -- r-addr f-addr r-cc f- cc)
-
     ; flash strings are zero-padded at the last cell
     ; that means: if the flash cell is less $0100, than mask the
     ; high byte in the ram cell
@@ -70,3 +72,40 @@ PFA_ICOMPARE_DONE:
     .dw XT_DOLITERAL
     .dw -1
     .dw XT_EXIT
+
+.ifdef WANT_IGNORECASE == 1
+XT_ICOMPARE_LC:
+    .dw DO_COLON
+PFA_ICOMPARE_LC:
+    .dw XT_DUP
+    .dw XT_DOLITERAL
+    .dw $00ff
+    .dw XT_AND
+    .dw XT_TOLOWER
+    .dw XT_SWAP
+    .dw XT_BYTESWAP
+    .dw XT_DOLITERAL
+    .dw $00ff
+    .dw XT_AND
+    .dw XT_TOLOWER
+    .dw XT_BYTESWAP
+    .dw XT_OR
+    .dw XT_EXIT
+    
+XT_TOLOWER:
+    .dw DO_COLON
+PFA_TOLOWER:
+    .dw XT_DUP
+    .dw XT_DOLITERAL
+    .dw 'A'
+    .dw XT_DOLITERAL
+    .dw 'Z'
+    .dw XT_WITHIN
+    .dw XT_DOCONDBRANCH
+    .dw PFA_TOLOWER0 
+    .dw XT_DOLITERAL
+    .dw $20 
+    .dw XT_OR 
+PFA_TOLOWER0:
+    .dw XT_EXIT 
+.endif
