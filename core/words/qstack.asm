f8f03a5bfeb2e47161e4b379350293c8d7848ece
mtrute <mtrute@1be2a6a9-e81f-0410-9be0-e59bd151596a>
2011-05-30 15:27:49 +0000

    Exception -42 is used otherwise, use the -4 standard definition instead
    git-svn-id: https://amforth.svn.sourceforge.net/svnroot/amforth/trunk@1064 1be2a6a9-e81f-0410-9be0-e59bd151596a

--- core/words/qstack.asm
+++ core/words/qstack.asm
@@ -1,6 +1,6 @@
 ; ( --  ) 
 ; Tools
-; check stack underflow, throw exception -42
+; check stack underflow, throw exception -4
 VE_QSTACK:
     .dw $ff06
     .db "?stack"
@@ -10,14 +10,14 @@ XT_QSTACK:
     .dw DO_COLON
 PFA_QSTACK:
 ; : ?stack ( -- )
-;    depth 0< if -&42 throw then
+;    depth 0< if -&4 throw then
 ; ;
     .dw XT_DEPTH
     .dw XT_LESSZERO
     .dw XT_DOCONDBRANCH
     .dw PFA_QSTACK1
       .dw XT_DOLITERAL
-      .dw -42
+      .dw -4
       .dw XT_THROW
 PFA_QSTACK1:
     .dw XT_EXIT
