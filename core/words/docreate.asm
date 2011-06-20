d738e587da105d069f0bff179e0d75d647605d2b
mtrute <mtrute@1be2a6a9-e81f-0410-9be0-e59bd151596a>
2011-06-20 14:20:33 +0000

    factor header creation into HEADER.
    git-svn-id: https://amforth.svn.sourceforge.net/svnroot/amforth/trunk@1093 1be2a6a9-e81f-0410-9be0-e59bd151596a

--- core/words/docreate.asm
+++ core/words/docreate.asm
@@ -1,6 +1,6 @@
-; ( --  ) (C: "<spaces>name" -- )
+; ( --  ) (C: "<spaces>name" -- a-addr )
 ; Compiler
-; creates the vocabulary header without XT and data field (PF)
+; parse the input and create an vocabulary entry without XT and data field (PF)
 ;VE_DOCREATE:
 ;    .dw $ff08
 ;    .db "(create)",0
@@ -9,25 +9,44 @@
 XT_DOCREATE:
     .dw DO_COLON
 PFA_DOCREATE:
-    .dw XT_DP     ; ( -- dp)
-    .dw XT_PARSENAME
-    .dw XT_DUP    ; ( -- dp addr' n n )
-    .dw XT_GREATERZERO ;( -- dp addr' n f )
+    .dw XT_PARSENAME 
+    .dw XT_GET_CURRENT 
+    .dw XT_FETCHE        
+    .dw XT_HEADER
+    .dw XT_EXIT
+
+; ( wid addr len -- a-addr )
+; Compiler
+; creates the vocabulary header without XT and data field (PF) in the wordlist wid
+VE_HEADER:
+    .dw $ff06
+    .db "header"
+    .dw VE_HEAD
+    .set VE_HEAD = VE_HEADER
+XT_HEADER:
+    .dw DO_COLON
+PFA_HEADER:
+    .dw XT_DP     
+    .dw XT_TO_R
+    .dw XT_TO_R
+    .dw XT_DUP    
+    .dw XT_GREATERZERO 
     .dw XT_DOCONDBRANCH
-    .dw PFA_DOCREATE4 ; ( -- dp addr' n )
+    .dw PFA_HEADER1
     .dw XT_DUP
     .dw XT_DOLITERAL
     .dw $ff00
     .dw XT_OR
     .dw XT_DOSCOMMA
     ; make voc link
-    .dw XT_GET_CURRENT ; ( -- dp head )
-    .dw XT_FETCHE
-    .dw XT_COMMA       ; ( -- dp current)
+    .dw XT_R_FROM
+    .dw XT_COMMA       
+    .dw XT_R_FROM
     .dw XT_EXIT
 
-PFA_DOCREATE4:
+PFA_HEADER1:
     ; -16: attempt to use zero length string as a name
     .dw XT_DOLITERAL
     .dw -16
     .dw XT_THROW
+
