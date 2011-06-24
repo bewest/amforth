84ef51c5e0a874b230b07ee8120c0a3d7b30969e
mtrute <mtrute@1be2a6a9-e81f-0410-9be0-e59bd151596a>
2011-06-24 13:59:55 +0000

    throw an exception until the code is ready
    git-svn-id: https://amforth.svn.sourceforge.net/svnroot/amforth/trunk@1099 1be2a6a9-e81f-0410-9be0-e59bd151596a

--- core/words/store-i_nvm.asm
+++ core/words/store-i_nvm.asm
@@ -2,14 +2,14 @@
 ; Memory
 ; writes n to flash at f-addr using NVM (ATXmega)
 VE_DO_STOREI_NVM:
-    .dw $ff07
-    .db "(!i-nvm)",0
+    .dw $ff08
+    .db "(!i-nvm)"
     .dw VE_HEAD
     .set VE_HEAD = VE_DO_STOREI_NVM
 XT_DO_STOREI:
     .dw DO_COLON
 PFA_DO_STOREI_NVM:
-    .dw XT_DROP
-    .dw XT_DROP
-    .dw XT_EXIT
+    .dw XT_DOLITERAL
+    .dw -20
+    .dw XT_THROW
 
