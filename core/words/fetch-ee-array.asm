6000822ce94fc8de35def85a7605232e8dd9fae6
mtrute <mtrute@1be2a6a9-e81f-0410-9be0-e59bd151596a>
2011-06-24 14:02:26 +0000

    factor array fetch/store operation.
    git-svn-id: https://amforth.svn.sourceforge.net/svnroot/amforth/trunk@1101 1be2a6a9-e81f-0410-9be0-e59bd151596a

--- core/words/fetch-ee-array.asm
+++ core/words/fetch-ee-array.asm
@@ -1,14 +1,14 @@
 ; ( ee-addr -- itemn .. item0 n) 
 ; Tools
-; Get an array of cells from EEPROM
-VE_GET_EE_ARRAY:
-    .dw $ff0b
-    .db "get-e-array",0
+; Get an array from EEPROM
+VE_FETCH_EE_ARRAY:
+    .dw $ff04
+    .db "@e[]"
     .dw VE_HEAD
-    .set VE_HEAD = VE_GET_EE_ARRAY
-XT_GET_EE_ARRAY:
+    .set VE_HEAD = VE_FETCH_EE_ARRAY
+XT_FETCH_EE_ARRAY:
     .dw DO_COLON
-PFA_GET_EE_ARRAY:
+PFA_FETCH_EE_ARRAY:
     .dw XT_DUP
     .dw XT_FETCHE
     .dw XT_TO_R    ; save the counter to r-stack
@@ -16,8 +16,8 @@ PFA_GET_EE_ARRAY:
     .dw XT_ZERO
     .dw XT_SWAP    ; go from bigger to smaller addresses
     .dw XT_DOQDO
-    .dw PFA_GET_EE_ARRAY2
-PFA_GET_EE_ARRAY1:
+    .dw PFA_FETCH_EE_ARRAY2
+PFA_FETCH_EE_ARRAY1:
     ; ( ee-addr )
     .dw XT_I
     .dw XT_CELLS ; ( -- ee-addr i*2 )
@@ -27,9 +27,10 @@ PFA_GET_EE_ARRAY1:
     .dw XT_SWAP   ;( -- item_i ee-addr )
     .dw XT_TRUE  ; shortcut for -1
     .dw XT_DOPLUSLOOP
-    .dw PFA_GET_EE_ARRAY1
-PFA_GET_EE_ARRAY2:
+    .dw PFA_FETCH_EE_ARRAY1
+PFA_FETCH_EE_ARRAY2:
     .dw XT_DROP
     .dw XT_DROP
     .dw XT_R_FROM ; get the counter from r-stack
     .dw XT_EXIT
+
--- core/words/set-order.asm
+++ core/words/set-order.asm
@@ -9,22 +9,8 @@ VE_SET_ORDER:
 XT_SET_ORDER:
     .dw DO_COLON
 PFA_SET_ORDER:
-    .dw XT_DUP
     .dw XT_DOLITERAL
     .dw EE_ORDERLISTLEN
-    .dw XT_STOREE
-    
-    .dw XT_ZERO
-    .dw XT_DODO
-    .dw PFA_SETORDER2
-PFA_SETORDER1:
-    .dw XT_DOLITERAL
-    .dw EE_ORDERLIST
-    .dw XT_I
-    .dw XT_2STAR
-    .dw XT_PLUS
-    .dw XT_STOREE
-    .dw XT_DOLOOP
-    .dw PFA_SETORDER1
-PFA_SETORDER2:
+    .dw XT_STORE_EE_ARRAY
     .dw XT_EXIT
+
