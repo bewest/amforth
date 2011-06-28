b81a575e75d14d99c0b19b7e2c208cb0d8ff4009
mtrute <mtrute@1be2a6a9-e81f-0410-9be0-e59bd151596a>
2011-06-28 18:41:48 +0000

    atxmegas use no interrupts for now; extracting usart module memory layout
    git-svn-id: https://amforth.svn.sourceforge.net/svnroot/amforth/trunk@1107 1be2a6a9-e81f-0410-9be0-e59bd151596a

--- appl/atxmega/atxmega.asm
+++ appl/atxmega/atxmega.asm
@@ -5,15 +5,12 @@
   .equ TIBSIZE  = $64    ; 80 characters is one line...
   .equ APPUSERSIZE = 10  ; size of user area
 
+.set WANT_ISR_TX = 0
+.set WANT_ISR_RX = 0
+
   .equ TERM_USART = USARTE0_base
   .equ TERM_USART_PORT = PORTE_base
 
-  .equ USART_DATA = TERM_USART + USART_DATA_offset
-  .equ USART_A = TERM_USART + USART_CTRLA_offset
-  .equ USART_B = TERM_USART + USART_CTRLB_offset
-  .equ USART_C = TERM_USART + USART_CTRLC_offset
-  .equ BAUDRATE_HIGH = TERM_USART + USART_BAUDCTRLB_offset
-  .equ BAUDRATE_LOW = TERM_USART + USART_BAUDCTRLA_offset
 ;  .equ TERM_USART_DreAddr = USARTE0_DRE_vect
 ;  .equ URXCAddr = USARTE0_RXC_vect
 
