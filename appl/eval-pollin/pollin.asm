2d3f091508487622dacd97dbe55f4d92e602c5fb
mtrute <mtrute@1be2a6a9-e81f-0410-9be0-e59bd151596a>
2012-01-23 18:51:19 +0000

    dynamically create the master control file during builds from a template and some settings from the build control file.
    git-svn-id: https://amforth.svn.sourceforge.net/svnroot/amforth/trunk@1145 1be2a6a9-e81f-0410-9be0-e59bd151596a

--- appl/eval-pollin/pollin.asm
+++ appl/eval-pollin/pollin.asm
@@ -1,4 +1,4 @@
-; Settings for the eval board with Atmega32 & 8 MHz
+; Settings for the pollin eval boards with various atmega's
 .include "macros.asm"
 .include "device.asm"
 
@@ -6,17 +6,22 @@
   .equ APPUSERSIZE = 10  ; size of user area
 
 ; cpu clock in hertz
-.equ F_CPU = 8000000
+.equ F_CPU = @F_CPU@
 ; baud rate of terminal
-.include "drivers/usart.asm"
+.equ BAUD = 9600
+.include "drivers/usart@USART@.asm"
 .equ USART_B_VALUE = bm_ENABLE_TX | bm_ENABLE_RX | bm_ENABLE_INT_RX
 .equ USART_C_VALUE = bm_ASYNC | bm_NO_PARITY | bm_1STOPBIT | bm_8BIT
-.equ BAUD = 9600
 
 .set rstackstart = RAMEND
 .set stackstart  = RAMEND - 80
-
 .set amforth_interpreter = max_dict_addr
-.set NUMWORDLISTS = 8
-.include "amforth.asm"
 
+.set NUMWORDLISTS = 8
+.set WANT_TIMER_COUNTER_0 = 1
+.set WANT_PORTA = 1
+.set WANT_PORTB = 1
+.set WANT_PORTC = 1
+.set WANT_PORTD = 1
+.set WANT_TWI = 1
+.include "amforth.asm"
