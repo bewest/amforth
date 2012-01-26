620c41cf58f5af81704518bee08da3c3039e8e0a
mtrute <mtrute@1be2a6a9-e81f-0410-9be0-e59bd151596a>
2012-01-26 18:53:58 +0000

    minor code compacting and improved macro usage
    git-svn-id: https://amforth.svn.sourceforge.net/svnroot/amforth/trunk@1148 1be2a6a9-e81f-0410-9be0-e59bd151596a

--- core/words/no-wdt.asm
+++ core/words/no-wdt.asm
@@ -9,7 +9,6 @@ VE_NOWDT:
 XT_NOWDT:
     .dw PFA_NOWDT
 PFA_NOWDT:
-     push temp1
      in temp1,SREG
      push temp1
      ; Turn always off global interrupt.
@@ -22,14 +21,13 @@ PFA_NOWDT:
      out MCUSR, temp1
      ; Write logical one to WDCE and WDE
      ; Keep old prescaler setting to prevent unintentional time-out
-     lds temp1, WDTCSR
+     in_ temp1, WDTCSR
      ori temp1, (1<<WDCE) | (1<<WDE)
-     sts WDTCSR, temp1
+     out_ WDTCSR, temp1
      ; Turn off WDT
      ldi temp1, (0<<WDE)
-     sts WDTCSR, temp1
+     out_ WDTCSR, temp1
      ; restore status register
      pop temp1
      out SREG,temp1
-     pop temp1
      jmp_ DO_NEXT
