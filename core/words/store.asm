d717307e1b27415b1a45e2371c4c010d20a2821f
mtrute <mtrute@1be2a6a9-e81f-0410-9be0-e59bd151596a>
2012-03-17 13:05:24 +0000

    unified store operation.
    git-svn-id: https://amforth.svn.sourceforge.net/svnroot/amforth/trunk@1180 1be2a6a9-e81f-0410-9be0-e59bd151596a

--- core/words/store.asm
+++ core/words/store.asm
@@ -9,6 +9,16 @@ VE_STORE:
 XT_STORE:
     .dw PFA_STORE
 PFA_STORE:
+.if WANT_UNIFIED == 1
+    ldi  zh, high(RAMEND)
+    ldi  zl, low(RAMEND)
+    cp  tosl, zl
+    cpc tosh, zh
+    brlt PFA_STORERAM
+    brbs 1, PFA_STORERAM
+    rjmp PFA_STOREOTHER
+.endif
+PFA_STORERAM:
     movw zl, tosl
     loadtos
     ; the high byte is written before the low byte
@@ -16,3 +26,10 @@ PFA_STORE:
     std Z+0, tosl
     loadtos
     jmp_ DO_NEXT
+.if WANT_UNIFIED == 1
+PFA_STOREOTHER:
+    adiw zl, 1
+    sub tosl, zl
+    sbc tosh, zh
+    jmp_ PFA_STOREE
+.endif
