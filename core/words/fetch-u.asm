3b0327164ccb113d79d669354543c8c0238c0df2
mtrute <mtrute@1be2a6a9-e81f-0410-9be0-e59bd151596a>
2012-03-23 19:35:30 +0000

    smaller code size with colon words
    git-svn-id: https://amforth.svn.sourceforge.net/svnroot/amforth/trunk@1189 1be2a6a9-e81f-0410-9be0-e59bd151596a

--- core/words/fetch-u.asm
+++ core/words/fetch-u.asm
@@ -7,12 +7,9 @@ VE_FETCHU:
     .dw VE_HEAD
     .set VE_HEAD = VE_FETCHU
 XT_FETCHU:
-    .dw PFA_FETCHU
+    .dw DO_COLON
 PFA_FETCHU:
-    movw zl, tosl
-    add  zl, upl
-    adc  zh, uph
-    ; low byte is read before the high byte
-    ld tosl, z+
-    ld tosh, z+
-    jmp_ DO_NEXT
+    .dw XT_UP_FETCH
+    .dw XT_PLUS
+    .dw XT_FETCH
+    .dw XT_EXIT
--- core/words/store-u.asm
+++ core/words/store-u.asm
@@ -7,14 +7,9 @@ VE_STOREU:
     .dw VE_HEAD
     .set VE_HEAD = VE_STOREU
 XT_STOREU:
-    .dw PFA_STOREU
+    .dw DO_COLON
 PFA_STOREU:
-    movw zl, tosl
-    add  zl, upl
-    adc  zh, uph
-    loadtos
-    ; the high byte is written before the low byte
-    std Z+1, tosh
-    std Z+0, tosl
-    loadtos
-    jmp_ DO_NEXT
+    .dw XT_UP_FETCH
+    .dw XT_PLUS
+    .dw XT_STORE
+    .dw XT_EXIT
