b17fd8a086c202bb603a8e3e619514900482de3d
mtrute <mtrute@1be2a6a9-e81f-0410-9be0-e59bd151596a>
2012-03-23 18:49:43 +0000

    complete redesign, all in one line, no addresses only the stack content. More like other forths and better looking in the call tracer.
    git-svn-id: https://amforth.svn.sourceforge.net/svnroot/amforth/trunk@1184 1be2a6a9-e81f-0410-9be0-e59bd151596a

--- core/words/dot-s.asm
+++ core/words/dot-s.asm
@@ -9,26 +9,18 @@ VE_DOTS:
 XT_DOTS:
     .dw DO_COLON
 PFA_DOTS:
+    .dw XT_SP0
     .dw XT_SP_FETCH
-    .dw XT_DEPTH
-    .dw XT_1MINUS
-    .dw XT_ZERO
+    .dw XT_CELLPLUS
     .dw XT_DOQDO
     .dw PFA_DOTS2
 PFA_DOTS1:
-    .dw XT_DUP
     .dw XT_I
-    .dw XT_DUP
-    .dw XT_UDOT
-    .dw XT_2STAR
-    .dw XT_PLUS
-    .dw XT_DUP
-    .dw XT_UDOT
     .dw XT_FETCH 
     .dw XT_UDOT
-    .dw XT_CR
-    .dw XT_DOLOOP
+    .dw XT_DOLITERAL
+    .dw 2
+    .dw XT_DOPLUSLOOP
     .dw PFA_DOTS1
 PFA_DOTS2:
-    .dw XT_DROP
     .dw XT_EXIT
